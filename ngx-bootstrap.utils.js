ef170dde6b47ba8f16c070461255e81b33e47497
Minh Van <minh.van1789@gmail.com>
2016-02-29 22:39:03 +0700

    Don't need ngxBootstrap.inherit any more

--- ngx-bootstrap.utils.js
+++ ngx-bootstrap.utils.js
@@ -8,49 +8,6 @@
 
     return target;
   };
-  
-  ngxBootstrap.inherit = function (target, source, overrideTarget) {
-    var _ngComponentLifeCycleFuncs = [
-      'constructor',
-      'ngOnChanges', 'ngOnInit', 'ngDoCheck', 
-      'ngAfterContentInit', 'ngAfterContentChecked', 
-      'ngAfterViewInit', 'ngAfterViewChecked',
-      'ngOnDestroy'
-    ]; 
-
-    target.__proto__.base = source;
-
-    for (var prop in source) {
-      if (prop == _ngComponentLifeCycleFuncs[0] || !target[prop] || (target[prop] && overrideTarget)) {
-        target.__proto__[prop] = source[prop];
-        
-        if (_ngComponentLifeCycleFuncs.indexOf(prop) > -1) {
-          if (prop == _ngComponentLifeCycleFuncs[0]) {
-            target[prop] = source[prop];
-            target.onConstructing = (function (constructor) {
-              return function () {
-                if (typeof constructor == 'function') {
-                  constructor.apply(source, arguments);
-                }
-                else {
-                  constructor[constructor.length - 1].apply(source, arguments);
-                }
-              }
-            })(target.__proto__[prop]);
-          }
-          else {
-            target[prop] = (function (func) {
-              return function () {
-                func.apply(source, arguments);
-              };
-            })(target.__proto__[prop]);
-          }
-        }
-      }
-    }
-
-    return target;
-  };
 
   ngxBootstrap.getRootInstance = function (instance) {
     if (instance && instance.__proto__.base) {

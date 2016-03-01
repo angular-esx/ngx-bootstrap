f504cb43f5c0e6f344f35c37d6afcbf7ce133388
Minh Van <minh.van1789@gmail.com>
2016-03-02 00:05:52 +0700

    "custom-label-color" test case for ngxLabelComponent

---
+++ components/label/tests/ui/custom-label-color/test-case.js
@@ -0,0 +1,30 @@
+﻿(function (testCases, ngxBootstrap, fileService) {
+  testCases.customLabelColor = ng.core.Component({
+    selector: 'ngx-test-case',
+    templateUrl: fileService.getTestCaseTemplate('label', 'custom-label-color'),
+    directives: [
+      ngxBootstrap.ngxComponents.ngxLabelComponent
+    ],
+    providers: [
+      ng.core.provide(ngxBootstrap.ngxComponents.ngxLabelService, { useClass: ng.core.Class(new customLabelService()) })
+    ]
+  })
+  .Class(new testCase());
+
+  function testCase() {
+    this.constructor = [ngxBootstrap.ngxComponents.ngxLabelService, function (ngxLabelService) {
+      this.COLORS = ngxLabelService.getColors();
+    }];
+  };
+
+  function customLabelService() {
+    ngxBootstrap.shallowCopy(this, new ngxBootstrap.ngxClass.ngxLabelServiceClass(), true);
+
+    this.getDefaultColorClass = function () {
+      return 'custom-label-default';
+    };
+    this.getOrangeColorClass = function () {
+      return 'custom-label-orange';
+    };
+  }
+})(window.testCases || (window.testCases = {}), window.ngxBootstrap, ngxBootstrap.configs.fileService);

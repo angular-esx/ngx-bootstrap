06b3ec5b70dd73c28cd090c74ab41f0abbfdbf59
Minh Van <minh.van1789@gmail.com>
2016-03-07 23:08:56 +0700

    Added utility functions

--- ngx-bootstrap.utils.js
+++ ngx-bootstrap.utils.js
@@ -9,12 +9,48 @@
     return target;
   };
 
+  ngxBootstrap.isObject = function (target) {
+    return target && typeof (target) === 'object';
+  };
+  ngxBootstrap.isArray = function (target) {
+    return target && Object.prototype.toString.call(target) === '[object Array]';
+  };
+  ngxBootstrap.isFunction = function (target) {
+    return target && typeof (target) === "function";
+  };
 
+  ngxBootstrap.distinct = function (target, compareTo) {
+    if (!ngxBootstrap.isArray(target)) {
+      return target;
     }
 
+    var _value, _result = [];
+    ngxBootstrap.forEach(target, function (item) {
+      _value = compareTo ? compareTo(item) : item;
 
+      if (_result.indexOf(_value) == -1) {
+        _result.push(_value);
       }
+    });
 
+    return _result;
+  };
+  ngxBootstrap.forEach = function (target, callback) {
+    if (target && callback) {
+      if(ngxBootstrap.isObject(target)){
+        for (var prop in target) {
+          if (callback(target[prop], prop)) {
+            break;
+          }
+        }
+      }
+      else if(ngxBootstrap.isFunction(target)){
+        for (var i = 0, length = target.length; i < length; i++) {
+          if (callback(target[i], i)) {
+            break;
+          }
+        }
+      }
     }
   };
 
@@ -25,5 +61,6 @@
   ngxBootstrap.addClass = function (element, className) {
     element.className = ngxBootstrap.hasClass(element, className) ? element.className : className;
   };
+
 
 })(window.ngxBootstrap || (window.ngxBootstrap = {}))

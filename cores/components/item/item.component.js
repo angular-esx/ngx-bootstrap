a66ce17cf6765a3e69a254307da61ec18dd7e4e4
Minh Van <minh.van1789@gmail.com>
2016-03-19 00:12:03 +0700

    Created 2 core components ngx-link & ngx-item

---
+++ cores/components/item/item.component.js
@@ -0,0 +1,14 @@
+﻿(function (ngxBootstrap) {
+  ngxBootstrap.ngxClass.ngxItemClass = ngxItem;
+
+  ngxBootstrap.ngxCores.ngxItemComponent = ng.core.Component({
+    selector: 'ngx-item',
+    template: '<ng-content></ng-content>'
+  })
+  .Class(new ngxItem());
+
+  function ngxItem() {
+    this.constructor = function () { };
+  }
+
+})(window.ngxBootstrap);

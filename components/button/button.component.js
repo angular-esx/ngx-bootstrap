92c191ea65bc4dcd3761e3983b76cb8dbedf0c67
Minh Van <minh.van1789@gmail.com>
2016-03-22 20:41:06 +0700

    Created ngxButtonComponent

---
+++ components/button/button.component.js
@@ -0,0 +1,73 @@
+﻿(function (ngxBootstrap) {
+  ngxBootstrap.ngxClass.ngxButtonClass = ngxButton;
+
+  ngxBootstrap.ngxComponents.ngxButtonComponent = ng.core.Component({
+    selector: 'ngx-button',
+    template: _getNgxButtonTemplate(),
+    providers: [ngxBootstrap.ngxCores.ngxRendererService]
+  })
+  .Class(new ngxButton());
+
+  function ngxButton() {
+    var _ATTRIBUTES = {
+      COLOR: 'color',
+      TYPE: 'type',
+      SIZE: 'size',
+      STATE: 'state'
+    };
+
+    this.constructor = [
+      ng.core.ElementRef,
+      ngxBootstrap.ngxCores.ngxRendererService,
+      ngxBootstrap.ngxComponents.ngxButtonService,
+
+      function (elementRef, ngxRendererService, ngxButtonService) {
+        this.cssClass = 'btn';
+
+        this.elementRef = elementRef;
+        this.ngxRendererService = ngxRendererService.for(elementRef.nativeElement);
+        this.ngxButtonService = ngxButtonService;
+      }
+    ];
+
+    this.ngAfterContentInit = function () {
+      this.color = this.ngxRendererService.getElementAttribute(_ATTRIBUTES.COLOR);
+      this.type = this.ngxRendererService.getElementAttribute(_ATTRIBUTES.TYPE);
+      this.size = this.ngxRendererService.getElementAttribute(_ATTRIBUTES.SIZE);
+      this.state = this.ngxRendererService.getElementAttribute(_ATTRIBUTES.STATE);
+
+      this.isDisabled = this.ngxButtonService.isDisabledState(this.state);
+    };
+
+    this.ngAfterViewInit = function () {
+      var _className = this.cssClass + ' ' + this.ngxButtonService.combineColorWithType(this.color, this.type);
+      if (this.size) {
+        _className += ' ' + this.ngxButtonService.getSizeClass(this.size);
+      }
+      if (this.state) {
+        _className += ' ' + this.ngxButtonService.getStateClass(this.state);
+      }
+
+      var _self = this;
+      ngxBootstrap.forEach(_ATTRIBUTES, function (attribute) {
+        _self.ngxRendererService.removeElementAttribute(attribute);
+      });
+      this.ngxRendererService.removeElementAttribute('class');
+      
+      this.ngxRendererService.for(this.elementRef.nativeElement.firstChild);
+      this.ngxRendererService.addToElementAttribute('class', _className, true);
+
+      this.ngxRendererService.for(this.elementRef.nativeElement);
+    };
+
+  }
+
+  function _getNgxButtonTemplate() {
+    return [
+      '<button type="button" [disabled]="isDisabled">',
+        '<ng-content></ng-content>',
+      '</button>'
+    ].join('');
+  }
+
+})(window.ngxBootstrap);
---
+++ components/button/services/button.service.js
@@ -0,0 +1,41 @@
+﻿(function (ngxBootstrap) {
+  ngxBootstrap.ngxClass.ngxButtonServiceClass = ngxButtonService;
+  ngxBootstrap.ngxComponents.ngxButtonService = ng.core.Class(new ngxButtonService());
+
+  function ngxButtonService() {
+    this.constructor = [
+      ngxBootstrap.ngxCores.ngxColorService,
+      ngxBootstrap.ngxCores.ngxTypeService,
+      ngxBootstrap.ngxCores.ngxSizeService,
+      ngxBootstrap.ngxCores.ngxStateService,
+
+      function (ngxColorService, ngxTypeService, ngxSizeService, ngxStateService) {
+        ngxBootstrap.shallowCopy(this, ngxColorService);
+        ngxBootstrap.shallowCopy(this, ngxTypeService);
+        ngxBootstrap.shallowCopy(this, ngxSizeService);
+        ngxBootstrap.shallowCopy(this, ngxStateService);
+
+        this.setPrefix('btn');
+      }
+    ];
+
+    this.getSmallBlockSizeClass = function () {
+      return this.getSmallSizeClass() + ' ' + this.getBlockSizeClass();
+    };
+    this.getLargeBlockSizeClass = function () {
+      return this.getLargeSizeClass() + ' ' + this.getBlockSizeClass();
+    };
+
+    this.isDisabledState = function (state) {
+      return state && state === this.getStates().DISABLED;
+    };
+
+    this.combineColorWithType = function (color, type) {
+      var _typeClass = this.getTypeClass(type);
+      var _colorClass = this.getColorClass(color);
+
+      return _typeClass ? _colorClass + '-' + _typeClass : _colorClass;
+    };
+  }
+
+})(window.ngxBootstrap);

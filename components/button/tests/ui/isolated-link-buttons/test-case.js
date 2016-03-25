9df1c923edce5af4913e113d7099a0a8a22fc5b6
Minh Van <minh.van1789@gmail.com>
2016-03-26 03:32:19 +0700

    Created "isolated components" test case for ngxLinkButton

---
+++ components/button/tests/ui/isolated-link-buttons/test-case.js
@@ -0,0 +1,28 @@
+﻿(function (testCases, cores, components, fileService) {
+  testCases.isolatedLinkButtons = ng.core.Component({
+    selector: 'ngx-test-case',
+    templateUrl: fileService.getTestCaseTemplate('button', 'isolated-link-buttons'),
+    directives: [
+      components.ngxLinkButtonComponent
+    ],
+    providers: [
+      cores.ngxColorService,
+      cores.ngxTypeService,
+      cores.ngxSizeService,
+      cores.ngxStateService,
+      components.ngxButtonService
+    ]
+  })
+  .Class(new testCase());
+
+  function testCase() {
+    this.constructor = [components.ngxButtonService, function (ngxButtonService) {
+      this.href = "https://www.google.com";
+
+      this.COLORS = ngxButtonService.getColors();
+      this.TYPES = ngxButtonService.getTypes();
+      this.SIZES = ngxButtonService.getSizes();
+      this.STATES = ngxButtonService.getStates();
+    }];
+  }
+})(window.testCases || (window.testCases = {}), window.ngxBootstrap.ngxCores, window.ngxBootstrap.ngxComponents, ngxBootstrap.configs.fileService);

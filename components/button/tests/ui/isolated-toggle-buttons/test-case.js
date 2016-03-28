9fa80d2fc6e7288f69fd89c43ac189f59494f9cb
Minh Van <minh.van1789@gmail.com>
2016-03-28 21:50:59 +0700

    Created "isolated-toggle-buttons" test case for ngxToggleButton

---
+++ components/button/tests/ui/isolated-toggle-buttons/test-case.js
@@ -0,0 +1,30 @@
+﻿(function (testCases, cores, components, fileService) {
+  testCases.isolatedToggleButtons = ng.core.Component({
+    selector: 'ngx-test-case',
+    templateUrl: fileService.getTestCaseTemplate('button', 'isolated-toggle-buttons'),
+    directives: [
+      components.ngxToggleButtonComponent
+    ],
+    providers: [
+      cores.ngxColorService,
+      cores.ngxTypeService,
+      cores.ngxSizeService,
+      cores.ngxStateService,
+      components.ngxButtonService
+    ]
+  })
+  .Class(new testCase());
+
+  function testCase() {
+    this.constructor = [components.ngxButtonService, function (ngxButtonService) {
+      this.COLORS = ngxButtonService.getColors();
+      this.TYPES = ngxButtonService.getTypes();
+      this.SIZES = ngxButtonService.getSizes();
+      this.STATES = ngxButtonService.getStates();
+    }];
+
+    this.onToggled = function (event) {
+      console.log(event);
+    };
+  }
+})(window.testCases || (window.testCases = {}), window.ngxBootstrap.ngxCores, window.ngxBootstrap.ngxComponents, ngxBootstrap.configs.fileService);

879259eb911cd76d3eb516c1194c6718cd2f83f8
Minh Van <minh.van1789@gmail.com>
2016-04-04 22:56:03 +0700

    Allowed to set state attribute to ngxItem

--- cores/components/item/item.component.js
+++ cores/components/item/item.component.js
@@ -3,12 +3,37 @@
 
   ngxBootstrap.ngxCores.ngxItemComponent = ng.core.Component({
     selector: 'ngx-item',
-    template: '<ng-content></ng-content>'
+    template: '<ng-content></ng-content>',
+    providers: [ngxBootstrap.ngxCores.ngxRendererService]
   })
   .Class(new ngxItem());
 
   function ngxItem() {
-    this.constructor = function () { };
+    var _ATTRIBUTES = {
+      STATE: 'state'
+    };
+
+    this.constructor = [
+     ng.core.ElementRef,
+     ngxBootstrap.ngxCores.ngxRendererService,
+     ngxBootstrap.ngxCores.ngxItemService,
+
+     function (elementRef, ngxRendererService, ngxItemService) {
+       this.elementRef = elementRef;
+       this.ngxRendererService = ngxRendererService.for(elementRef.nativeElement);
+       this.ngxItemService = ngxItemService;
+     }
+    ];
+
+    this.ngAfterContentInit = function () {
+      this.state = this.ngxRendererService.getElementAttribute(_ATTRIBUTES.STATE);
+    };
+
+    this.ngAfterViewInit = function () {
+      if (this.state) {
+        this.ngxRendererService.addToElementAttribute('class', this.ngxItemService.getStateClass(this.state), true);
+      }
+    };
   }
 
 })(window.ngxBootstrap);

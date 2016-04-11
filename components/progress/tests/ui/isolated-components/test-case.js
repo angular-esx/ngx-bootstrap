761f6c3cb5051cecdec330456c12767058955d00
Minh Van <minh.van1789@gmail.com>
2016-04-11 21:49:07 +0700

    Created "isolated-components" test case for ngxProgress

---
+++ components/progress/tests/ui/isolated-components/test-case.js
@@ -0,0 +1,22 @@
+﻿(function (testCases, cores, components, fileService) {
+  testCases.isolatedComponents = ng.core.Component({
+    selector: 'ngx-test-case',
+    templateUrl: fileService.getTestCaseTemplate('progress'),
+    directives: [
+      components.ngxProgressComponent
+    ],
+    providers: [
+      cores.ngxColorService,
+      cores.ngxTypeService,
+      components.ngxProgressService
+    ]
+  })
+  .Class(new testCase());
+
+  function testCase() {
+    this.constructor = [components.ngxProgressService, function (ngxProgressService) {
+      this.COLORS = ngxProgressService.getColors();
+      this.TYPES = ngxProgressService.getTypes();
+    }];
+  }
+})(window.testCases || (window.testCases = {}), window.ngxBootstrap.ngxCores, window.ngxBootstrap.ngxComponents, ngxBootstrap.configs.fileService);

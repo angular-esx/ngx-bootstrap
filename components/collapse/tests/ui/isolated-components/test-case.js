8aa7ec252b1b2a8e589be5920c8482a4d0dfcbda
Thanh Tran <vanthanh2305@gmail.com>
2016-04-18 00:06:41 +0700

    Collapse

---
+++ components/collapse/tests/ui/isolated-components/test-case.js
@@ -0,0 +1,26 @@
+﻿(function (testCases, cores, components, fileService) {
+  testCases.isolatedComponents = ng.core.Component({
+    selector: 'ngx-test-case',
+    templateUrl: fileService.getTestCaseTemplate('collapse'),
+    directives: [
+      components.ngxCollapseComponent
+    ],
+    providers: [
+    ]
+  })
+  .Class(new testCase());
+
+  function testCase() {
+    this.constructor = function () {
+      var self = this;
+      setTimeout(function(){
+        self.isCollapse = true;
+        console.log('collapse');
+      }, 2000)
+    };
+
+    this.collapse = function() {
+      this.isCollapse = !this.isCollapse;
+    }
+  }
+})(window.testCases || (window.testCases = {}), window.ngxBootstrap.ngxCores, window.ngxBootstrap.ngxComponents, ngxBootstrap.configs.fileService);

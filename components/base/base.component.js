305b4120738e05384c8523665bff0b533dbf8391
Minh Van <minh.van1789@gmail.com>
2016-04-23 17:49:18 +0700

    Created ngxBaseComponent

---
+++ components/base/base.component.js
@@ -0,0 +1,24 @@
+﻿(function (ngxBootstrap) {
+  ngxBootstrap.ngxComponents.ngxBaseComponent = ng.core.Class(new _ngxBaseComponent());
+
+  function _ngxBaseComponent() {
+    this.constructor = [
+      ng.core.ElementRef,
+      ngxBootstrap.ngxCores.ngxRenderService,
+
+      function (elementRef, ngxRenderService) {
+        this.elementRef = elementRef;
+        
+        this.ngxRenderService = ngxRenderService ? ngxRenderService.for(elementRef.nativeElement) : null;
+      }
+    ];
+
+    this.getPrevPropertyValue = function (changeRecord, propertyName) {
+      return changeRecord[propertyName] ? (ngxBootstrap.isString(changeRecord[propertyName].previousValue) ? changeRecord[propertyName].previousValue : null) : this[propertyName];
+    };
+    this.getCurrentPropertyValue = function (changeRecord, propertyName) {
+      return changeRecord[propertyName] ? changeRecord[propertyName].currentValue : this[propertyName];
+    };
+  }
+
+})(window.ngxBootstrap);

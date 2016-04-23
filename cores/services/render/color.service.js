a3f9a5a82f680c2df10ce0ac92c17232575a732d
Minh Van <minh.van1789@gmail.com>
2016-04-23 17:48:41 +0700

    Refactored ngxColorService & ngxTypeService

--- cores/services/render/color.service.js
+++ cores/services/render/color.service.js
@@ -1,33 +1,27 @@
 ﻿(function (ngxBootstrap) {
-  ngxBootstrap.ngxClass.ngxColorServiceClass = ngxColorService;
-  ngxBootstrap.ngxCores.ngxColorService = ng.core.Class(new ngxColorService());
+  ngxBootstrap.ngxCores.ngxColorService = ng.core.Class(new _ngxColorService());
 
-  function ngxColorService() {
-    var _colors;
+  function _ngxColorService() {
+    var _COLORS;
 
     this.constructor = function () {
-    };
-
-    this.setPrefix = function (prefix) {
-      this.prefix = prefix ? prefix + '-' : '';
-      return this;
+      this.prefixClass = '';
     };
 
     this.getColorClass = function (color) {
-      if (!color) {
-        return this.getDefaultColorClass();
-      }
-      color = color.toLowerCase().replace(/-([a-z])/g, function (x, y) { return y.toUpperCase(); });
-      color = color.charAt(0).toUpperCase() + color.slice(1);
+      if (!color) { return ''; }
 
-      var _funcName = 'get' + color + 'ColorClass';
+      var _color = color.toLowerCase().replace(/-([a-z])/g, function (x, y) { return y.toUpperCase(); });
+      _color = _color.charAt(0).toUpperCase() + _color.slice(1);
 
-      return this[_funcName] ? this[_funcName]() : this.prefix + color;
+      var _funcName = 'get' + _color + 'ColorClass';
+
+      return this[_funcName] ? this[_funcName]() : this.prefixClass + '-' +_color;
     };
 
     this.getColors = function () {
-      if (!_colors) {
-        _colors = {};
+      if (!_COLORS) {
+        _COLORS = {};
         var _color;
 
         for (var prop in this) {
@@ -36,35 +30,112 @@
                           .replace(/([A-Z])/g, function (x, y) { return '_' + y; })
                           .replace(/^_/, '');
 
-            _colors[_color.toUpperCase()] = _color.toLocaleLowerCase().replace(/_/g, '-');
+            _COLORS[_color.toUpperCase()] = _color.toLocaleLowerCase().replace(/_/g, '-');
           }
         }
       }
 
-      return ngxBootstrap.shallowCopy({}, _colors);
+      return ngxBootstrap.shallowCopy({}, _COLORS);
     };
 
+    this.isDefaultColorClass = function (color) {
+      return this.getColorClass(color) === this.getDefaultColorClass();
+    };
     this.getDefaultColorClass = function () {
-      return this.prefix + 'default';
+      return this.prefixClass + '-default';
+    };
+
+    this.isDefaultOutlineColorClass = function (color) {
+      return this.getColorClass(color) === this.getDefaultOutlineColorClass();
+    };
+    this.getDefaultOutlineColorClass = function () {
+      return this.prefixClass + '-default-outline';
+    };
+
+    this.isPrimaryColorClass = function (color) {
+      return this.getColorClass(color) === this.getPrimaryColorClass();
     };
     this.getPrimaryColorClass = function () {
-      return this.prefix + 'primary'; 
+      return this.prefixClass + '-primary';
+    };
+
+    this.isPrimaryOutlineColorClass = function (color) {
+      return this.getColorClass(color) === this.getPrimaryOutlineColorClass();
+    };
+    this.getPrimaryOutlineColorClass = function () {
+      return this.prefixClass + '-primary-outline';
+    };
+
+    this.isSecondaryColorClass = function (color) {
+      return this.getColorClass(color) === this.getSecondaryColorClass();
     };
     this.getSecondaryColorClass = function () {
-      return this.prefix + 'secondary';
+      return this.prefixClass + '-secondary';
+    };
+
+    this.isSecondaryOutlineColorClass = function (color) {
+      return this.getColorClass(color) === this.getSecondaryOutlineColorClass();
+    };
+    this.getSecondaryOutlineColorClass = function () {
+      return this.prefixClass + '-secondary-outline';
+    };
+
+    this.isInfoColorClass = function (color) {
+      return this.getColorClass(color) === this.getInfoColorClass();
     };
     this.getInfoColorClass = function () {
-      return this.prefix + 'info';
+      return this.prefixClass + '-info';
+    };
+
+    this.isInfoOutlineColorClass = function (color) {
+      return this.getColorClass(color) === this.getInfoOutlineColorClass();
+    };
+    this.getInfoOutlineColorClass = function () {
+      return this.prefixClass + '-info-outline';
+    };
+
+    this.isSuccessColorClass = function (color) {
+      return this.getColorClass(color) === this.getSuccessColorClass();
     };
     this.getSuccessColorClass = function () {
-      return this.prefix + 'success';
+      return this.prefixClass + '-success';
+    };
+
+    this.isSuccessOutlineColorClass = function (color) {
+      return this.getColorClass(color) === this.getSuccessOutlineColorClass();
+    };
+    this.getSuccessOutlineColorClass = function () {
+      return this.prefixClass + '-success-outline';
+    };
+
+    this.isWarningColorClass = function (color) {
+      return this.getColorClass(color) === this.getWarningColorClass();
     };
     this.getWarningColorClass = function () {
-      return this.prefix + 'warning';
+      return this.prefixClass + '-warning';
+    };
+
+    this.isWarningOutlineColorClass = function (color) {
+      return this.getColorClass(color) === this.getWarningOutlineColorClass();
+    };
+    this.getWarningOutlineColorClass = function () {
+      return this.prefixClass + '-warning-outline';
+    };
+
+    this.isDangerColorClass = function (color) {
+      return this.getColorClass(color) === this.getDangerColorClass();
     };
     this.getDangerColorClass = function () {
-      return this.prefix + 'danger';
+      return this.prefixClass + '-danger';
     };
+
+    this.isDangerOutlineColorClass = function (color) {
+      return this.getColorClass(color) === this.getDangerOutlineColorClass();
+    };
+    this.getDangerOutlineColorClass = function () {
+      return this.prefixClass + '-danger-outline';
+    };
+
   }
 
 })(window.ngxBootstrap);
--- cores/services/render/type.service.js
+++ cores/services/render/type.service.js
@@ -1,31 +1,26 @@
 ﻿(function (ngxBootstrap) {
-  ngxBootstrap.ngxClass.ngxTypeServiceClass = ngxTypeService;
-  ngxBootstrap.ngxCores.ngxTypeService = ng.core.Class(new ngxTypeService());
+  ngxBootstrap.ngxCores.ngxTypeService = ng.core.Class(new _ngxTypeService());
 
-  function ngxTypeService() {
-    var _types;
+  function _ngxTypeService() {
+    var _TYPES;
 
     this.constructor = function () {
-    };
-
-    this.setPrefix = function (prefix) {
-      this.prefix = prefix ? prefix + '-' : '';
-      return this;
+      this.prefixClass = '';
     };
 
     this.getTypeClass = function (type) {
       if (!type) { return ''; }
 
-      type = type.toLowerCase().replace(/-([a-z])/g, function (x, y) { return y.toUpperCase(); });
-      type = type.charAt(0).toUpperCase() + type.slice(1);
+      var _type = type.toLowerCase().replace(/-([a-z])/g, function (x, y) { return y.toUpperCase(); });
+      _type = _type.charAt(0).toUpperCase() + _type.slice(1);
 
-      var _funcName = 'get' + type + 'TypeClass';
-      return this[_funcName] ? this[_funcName]() : this.prefix + type;
+      var _funcName = 'get' + _type + 'TypeClass';
+      return this[_funcName] ? this[_funcName]() : this.prefixClass + '-' +_type;
     };
 
     this.getTypes = function () {
-      if (!_types) {
-        _types = {};
+      if (!_TYPES) {
+        _TYPES = {};
         var _type;
 
         for (var prop in this) {
@@ -34,16 +29,13 @@
                         .replace(/([A-Z])/g, function (x, y) { return '_' + y; })
                         .replace(/^_/, '');
 
-            _types[_type.toUpperCase()] = _type.toLocaleLowerCase().replace(/_/g, '-');
+            _TYPES[_type.toUpperCase()] = _type.toLocaleLowerCase().replace(/_/g, '-');
           }
         }
       }
 
-      return ngxBootstrap.shallowCopy({}, _types);
+      return ngxBootstrap.shallowCopy({}, _TYPES);
     };
 
-    this.getOutlineTypeClass = function () {
-      return 'outline';
-    };
   }
 })(window.ngxBootstrap);

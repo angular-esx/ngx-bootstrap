a98d5dbda7823527cc72796c664fef9b071ab0cc
Minh Van <minh.van1789@gmail.com>
2016-04-23 17:47:46 +0700

    Renamed ngxRendererService to ngxRenderService and used BrowserDomAdapter inside of it instead of Renderer

---
+++ cores/services/render/render.service.js
@@ -0,0 +1,62 @@
+﻿(function (ngxBootstrap) {
+  ngxBootstrap.ngxCores.ngxRenderService = ng.core.Class(new _ngxRenderService());
+
+  function _ngxRenderService() {
+    this.constructor = function () {
+      this.domAdapter = new ng.platform.browser.BrowserDomAdapter();
+    };
+
+    this.for = function (nativeElement) {
+      this.nativeElement = nativeElement;
+
+      return this;
+    };
+
+    this.hasClass = function (className) {
+      return this.domAdapter.hasClass(this.nativeElement, className);
+    };
+    this.addClass = function (className) {
+      this.domAdapter.addClass(this.nativeElement, className);
+      return this;
+    };
+    this.insertClass = function (className, index) {
+      if (!className || (index !== 0 && !index) || isNaN(index)) { return this; }
+
+      var _currentClass = this.getAttribute('class');
+      if (_currentClass) {
+        var _classes = [];
+        ngxBootstrap.forEach(_currentClass.split(' '), function (item) {
+          if (item) { _classes.push(item); }
+        });
+
+        _classes.splice(index, 0, className);
+
+        this.setAttribute('class', _classes.join(' '));
+      }
+
+      return this;
+    };
+    this.removeClass = function (className) {
+      this.domAdapter.removeClass(this.nativeElement, className);
+      return this;
+    };
+
+
+    this.hasAttribute = function (attribute) {
+      return this.domAdapter.hasAttribute(this.nativeElement, attribute);
+    };
+    this.getAttribute = function (attribute) {
+      return this.domAdapter.getAttribute(this.nativeElement, attribute);
+    };
+    this.setAttribute = function (attribute, value) {
+      this.domAdapter.setAttribute(this.nativeElement, attribute, value);
+      return this;
+    };
+    this.removeAttribute = function (attribute) {
+      this.domAdapter.removeAttribute(this.nativeElement, attribute);
+      return this;
+    };
+
+  }
+
+})(window.ngxBootstrap);

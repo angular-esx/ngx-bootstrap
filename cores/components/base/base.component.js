654deb0dcd817f16e5839bdb7c21c879e84e6d6c
Minh Van <minh.van1789@gmail.com>
2016-05-09 15:49:30 +0700

    Updated ngxBaseComponent

--- cores/components/base/base.component.js
+++ cores/components/base/base.component.js
@@ -3,25 +3,124 @@ var ngxBootstrap = require('./../../ngx-bootstrap.js');
 ngxBootstrap = require('./../../ngx-bootstrap.utils.js');
 
 function _ngxBaseComponent() {
+  var _ATTRIBUTES = {
+    COLOR: 'color',
+    TYPE: 'type',
+    STATE: 'state',
+    SIZE: 'size'
+  };
+  
   this.constructor = [
     ng.core.ElementRef,
     ngxRenderService,
+    [new ng.core.Optional(), null],
 
-    function (elementRef, ngxRenderService) {
-      this.elementRef = elementRef;
+    function (elementRef, ngxRenderService, ngxBaseService) {
+      if(!ngxRenderService){ throw 'ngxRenderService is required'; }
       
-      this.ngxRenderService = ngxRenderService ? ngxRenderService.for(elementRef.nativeElement) : null;
+      this.elementRef = elementRef;
+      this.ngxRenderService = ngxRenderService.for(elementRef.nativeElement);
+      this.ngxBaseService = ngxBaseService;
     }
   ];
   
   this.ngOnChanges = function (changeRecord) {
-    this.cssClass = this.onBuildCssClass(changeRecord);
+    var _aggregate = this.onAggregatePropertyValueState(changeRecord);
+    
+    this.cssClass = (this.onBuildOwnCssClass(_aggregate).join(' ') + ' ' + this.onBuildExtendCssClass(_aggregate).join(' ')).trim();
+    
+    this.onSetCssClass();
   };
 
-  this.onBuildCssClass = function(changeRecord){ return ''; };
+  this.ngAfterViewInit = function(){
+    if((this.cssClass === null || this.cssClass === undefined) && (this.ngxBaseService && this.ngxBaseService.prefixClass)){
+      this.cssClass = this.ngxBaseService.prefixClass;
+      
+      this.ngxRenderService.insertClass(this.cssClass, 0);
+    }
+  };
+  
+  this.onAggregatePropertyValueState = function(changeRecord){
+    var _aggregate = {};
+    
+    if(!this.ngxBaseService){ return _aggregate; }
+    
+    if(this.ngxBaseService.getColorClass){
+      _aggregate[_ATTRIBUTES.COLOR] = {
+        prev: this.ngxBaseService.getColorClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.COLOR)),
+        current: this.ngxBaseService.getColorClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.COLOR))
+      };
+    }
+    
+    if(this.ngxBaseService.getTypeClass){
+      _aggregate[_ATTRIBUTES.TYPE] = {
+        prev: this.ngxBaseService.getTypeClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.TYPE)),
+        current: this.ngxBaseService.getTypeClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.TYPE))
+      };
+    }
+    
+    if(this.ngxBaseService.getStateClass){
+      _aggregate[_ATTRIBUTES.STATE] = {
+        prev: this.ngxBaseService.getStateClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.STATE)),
+        current: this.ngxBaseService.getStateClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.STATE))
+      };
+    }
+
+    if(this.ngxBaseService.getSizeClass){
+      _aggregate[_ATTRIBUTES.SIZE] = {
+        prev: this.ngxBaseService.getPositionClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.SIZE)),
+        current: this.ngxBaseService.getPositionClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.SIZE))
+      };
+    }
+
+    return _aggregate;
+  };
+
+  this.onBuildOwnCssClass = function(aggregate){
+    if(!this.ngxBaseService){ return []; }
+    
+    var _classes = [this.ngxBaseService.prefixClass];
+    
+    ngxBootstrap.forEach(aggregate, function(attribute){
+      if(attribute.current){ _classes.push(attribute.current); }
+    });
+    
+    return _classes;
+  };
+  
+  this.onBuildExtendCssClass = function(aggregate){
+    var _currentClass = this.ngxRenderService.getClass();
+    
+    if(!_currentClass){ return []; }
+    
+    var _prefixClass = this.ngxBaseService ? this.ngxBaseService.prefixClass : '',
+        _classes = [],
+        _isExistingCssClass = false;
+    
+    ngxBootstrap.forEach(_currentClass.split(' '), function (className) {
+      if(className && className != _prefixClass){
+        _isExistingCssClass = false;
+        
+        ngxBootstrap.forEach(aggregate, function(attribute){
+          if(attribute.prev && attribute.prev.indexOf(className) > -1){
+            _isExistingCssClass = true;
+            return true;
+          }
+        });
+        
+        if(!_isExistingCssClass){ _classes.push(className); }
+      }
+    });
+    
+    return _classes;
+  };
+
+  this.onSetCssClass = function(){
+    this.ngxRenderService.setClass(this.cssClass);
+  };
 
   this.getPrevPropertyValue = function (changeRecord, propertyName) {
-    return changeRecord[propertyName] ? (ngxBootstrap.isString(changeRecord[propertyName].previousValue) ? changeRecord[propertyName].previousValue : null) : this[propertyName];
+    return !changeRecord[propertyName] || changeRecord[propertyName].isFirstChange() ? this[propertyName] : changeRecord[propertyName].previousValue;
   };
   
   this.getCurrentPropertyValue = function (changeRecord, propertyName) {
@@ -30,7 +129,7 @@ function _ngxBaseComponent() {
   
   this.removeOneTimeBindingAttributes = function (attributes) {
     var _self = this,
-        _attributes = ngxBootstrap.isArray(attributes) ? attributes : [attributes];
+        _attributes = ngxBootstrap.isArray(attributes) || ngxBootstrap.isObject(attributes) ? attributes : [attributes];
         
     ngxBootstrap.forEach(_attributes, function (attribute) {
       _self.ngxRenderService.removeAttribute(attribute);

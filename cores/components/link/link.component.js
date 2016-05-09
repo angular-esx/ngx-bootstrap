92072dde8e88fddb08a99480cd38a672f34907cf
Minh Van <minh.van1789@gmail.com>
2016-05-09 15:51:30 +0700

    Applied ngxBaseComponent which has been updated to ngxLinkComponent and Supported important attributes of <a>

--- cores/components/link/link.component.js
+++ cores/components/link/link.component.js
@@ -7,6 +7,11 @@ ngxBootstrap = require('./../../ngx-bootstrap.utils.js');
 function _ngxLinkComponent() {
   var _ATTRIBUTES = {
     HREF: 'href',
+    HREF_LANG: 'hreflang',
+    MEDIA_QUERY: { name: 'media',  alias: 'media-query' },
+    MEDIA_TYPE: { name: 'type',  alias: 'media-type' },
+    REL: 'rel',
+    TARGET: 'target',
     COLOR: 'color',
     SIZE: 'size',
     STATE: 'state'
@@ -20,11 +25,10 @@ function _ngxLinkComponent() {
     ngxLinkService,
 
     function (elementRef, ngxRenderService, ngxLinkService) {
-      ngxBaseComponent.call(this, elementRef, ngxRenderService);
+      ngxBaseComponent.apply(this, arguments);
       
       this.base = Object.getPrototypeOf(Object.getPrototypeOf(this));
       this.ngxLinkService = ngxLinkService;
-      this.cssClass = this.ngxLinkService.prefixClass;
       
       if(!this.href){ this.href = '#'; }
       
@@ -32,52 +36,65 @@ function _ngxLinkComponent() {
     }
   ];
   
-  this.ngAfterViewInit = function () {
-    this.removeOneTimeBindingAttributes(_ATTRIBUTES.HREF);
+  this.ngOnChanges = function(changeRecord) {
+    this.base.ngOnChanges.apply(this, arguments);
+    
+    if(this.link){
+      var _property, _self = this; 
+      var _attributes = [
+        _ATTRIBUTES.HREF_LANG,
+        _ATTRIBUTES.MEDIA_QUERY.name,
+        _ATTRIBUTES.MEDIA_TYPE.name,
+        _ATTRIBUTES.REL,
+        _ATTRIBUTES.TARGET
+      ];
+      
+      this.ngxRenderService.for(this.link.nativeElement);
+
+      ngxBootstrap.forEach(_attributes, function (attribute) {
+        _property = changeRecord[attribute];
+
+        if (_property && (_property.isFirstChange() || _property.previousValue != _property.currentValue)) {
+          if (_property.currentValue) {
+            _self.ngxRenderService.setAttribute(attribute, _property.currentValue);
+          }
+          else {
+            _self.ngxRenderService.removeAttribute(attribute);
+          }
+        }
+      });
+
+      this.ngxRenderService.for(this.elementRef.nativeElement);
+    }
+    
   };
   
-  this.onBuildCssClass = function (changeRecord) {
-    var _prefixClass = this.ngxLinkService.prefixClass;
+  this.ngAfterViewInit = function () {
+    this.base.ngAfterViewInit.apply(this);
     
-    var _prevColor, _currentColor;
-    if(this.ngxLinkService.getColorClass){
-      _prevColor = this.ngxLinkService.getColorClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.COLOR));
-      _currentColor = this.ngxLinkService.getColorClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.COLOR));
-    }
+    var _self = this, _attributes = [
+      _ATTRIBUTES.HREF,
+      _ATTRIBUTES.HREF_LANG,
+      _ATTRIBUTES.MEDIA_QUERY.alias,
+      _ATTRIBUTES.MEDIA_TYPE.alias,
+      _ATTRIBUTES.REL,
+      _ATTRIBUTES.TARGET
+    ];
     
-    var _prevSize, _currentSize;
-    if(this.ngxLinkService.getSizeClass){
-      _prevSize = this.ngxLinkService.getSizeClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.SIZE));
-      _currentSize = this.ngxLinkService.getSizeClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.SIZE));
-    }
+    this.removeOneTimeBindingAttributes(_attributes);
     
-    var _prevState, _currentState;
-    if(this.ngxLinkService.getStateClass){
-      _prevState = this.ngxLinkService.getStateClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.STATE));
-      _currentState = this.ngxLinkService.getStateClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.STATE));
-    }
-
-    var _classes = [_prefixClass];
+    this.ngxRenderService.for(this.link.nativeElement);
+    _attributes.splice(0, 1);
     
-    if (_currentColor) { _classes.push(_currentColor); }
-    if (_currentSize) { _classes.push(_currentSize); }
-    if (_currentState) { _classes.push(_currentState); }
-      
-    ngxBootstrap.forEach(this.cssClass.split(' '), function (className) {
-      if (
-          className && className != _prefixClass &&
-          (!_prevColor || _prevColor.indexOf(className) === -1) &&
-          (!_prevSize || _prevSize.indexOf(className) === -1) &&
-          (!_prevState || _prevState.indexOf(className) === -1)
-        )
-      {
-        _classes.push(className);
+    ngxBootstrap.forEach(_attributes, function(attribute) {
+      if(_self[attribute]){
+        _self.ngxRenderService.setAttribute(attribute, _self[attribute]);
       }
     });
-
-    return _classes.join(' ');
+    
+    this.ngxRenderService.for(this.elementRef.nativeElement);
   };
-
+  
   this.click = function ($event) {
     if (this.ngxLinkService.isDisabledStateClass(this.state)) {
       $event.preventDefault();
@@ -96,8 +113,12 @@ function _ngxLinkComponent() {
 
 module.exports = ng.core.Component({
   selector: 'ngx-link',
-  template: '<a [class]="cssClass" [href]="href" (click)="click($event)"><ng-content></ng-content></a>',
-  properties: ['href', 'color', 'size', 'state'],
+  template: '<a #link [href]="href" (click)="click($event)"><ng-content></ng-content></a>',
+  providers: [ngxRenderService],
+  queries: {
+    link: new ng.core.ViewChild('link')
+  },
+  properties: ['href', 'hreflang', 'media-query', 'media-type', 'rel', 'target', 'color', 'size', 'state'],
   events: ['clickEmitter: click']
 })
 .Class(new _ngxLinkComponent());

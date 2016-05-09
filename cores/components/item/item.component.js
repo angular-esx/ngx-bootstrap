e5f082ed26e1b995311081f037b73035f70fb6d8
Minh Van <minh.van1789@gmail.com>
2016-05-09 15:50:25 +0700

    Applied ngxBaseComponent which has been updated to ngxItemComponent

--- cores/components/item/item.component.js
+++ cores/components/item/item.component.js
@@ -1,5 +1,6 @@
 ﻿var ngxItemService = require('./services/item.service.js');
 var ngxBaseComponent = require('./../base/base.component.js');
+var ngxRenderService = require('./../../services/render/render.service.js');
 var ngxBootstrap = require('./../../ngx-bootstrap.js');
 ngxBootstrap = require('./../../ngx-bootstrap.utils.js');
 
@@ -14,64 +15,22 @@ function _ngxItemComponent() {
 
   this.constructor = [
     ng.core.ElementRef,
+    ngxRenderService,
     ngxItemService,
 
-    function (elementRef, ngxItemService) {
-      ngxBaseComponent.call(this, elementRef);
+    function (elementRef, ngxRenderService, ngxItemService) {
+      ngxBaseComponent.apply(this, arguments);
       
       this.base = Object.getPrototypeOf(Object.getPrototypeOf(this));
       this.ngxItemService = ngxItemService;
-      this.cssClass = this.ngxItemService.prefixClass;
     }
   ];
-
-  this.onBuildCssClass = function (changeRecord) {
-    var _prefixClass = this.ngxItemService.prefixClass;
-    
-    var _prevColor, _currentColor;
-    if(this.ngxItemService.getColorClass){
-      _prevColor = this.ngxItemService.getColorClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.COLOR));
-      _currentColor = this.ngxItemService.getColorClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.COLOR));
-    }
-    
-    var _prevSize, _currentSize;
-    if(this.ngxItemService.getSizeClass){
-      _prevSize = this.ngxItemService.getSizeClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.SIZE));
-      _currentSize = this.ngxItemService.getSizeClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.SIZE));
-    }
-    
-    var _prevState, _currentState;
-    if(this.ngxItemService.getStateClass){
-      _prevState = this.ngxItemService.getStateClass(this.getPrevPropertyValue(changeRecord, _ATTRIBUTES.STATE));
-      _currentState = this.ngxItemService.getStateClass(this.getCurrentPropertyValue(changeRecord, _ATTRIBUTES.STATE));
-    }   
-
-    var _classes = [_prefixClass];
-    
-    if (_currentColor) { _classes.push(_currentColor); }
-    if (_currentSize) { _classes.push(_currentSize); }
-    if (_currentState) { _classes.push(_currentState); }
-      
-    ngxBootstrap.forEach(this.cssClass.split(' '), function (className) {
-      if (
-          className && className != _prefixClass &&
-          (!_prevColor || _prevColor.indexOf(className) === -1) &&
-          (!_prevSize || _prevSize.indexOf(className) === -1) &&
-          (!_prevState || _prevState.indexOf(className) === -1)
-        )
-      {
-        _classes.push(className);
-      }
-    });
-
-    return _classes.join(' ');
-  };
-  
 }
 
 module.exports = ng.core.Component({
   selector: 'ngx-item',
-  template: '<div [class]="cssClass"><ng-content></ng-content></div>',
+  template: '<ng-content></ng-content>',
+  providers: [ngxRenderService],
   properties: ['color', 'size', 'state']
 })
 .Class(new _ngxItemComponent());

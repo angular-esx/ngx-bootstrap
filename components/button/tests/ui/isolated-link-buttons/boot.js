6c03f021fb08ef7d9eb444d69e628823132716c3
Minh Van <minh.van1789@gmail.com>
2016-05-11 05:57:40 +0700

    Deleted unnecessary files

--- components/button/tests/ui/isolated-link-buttons/boot.js
+++
@@ -1,5 +0,0 @@
-(function (testCases) {
-    document.addEventListener('DOMContentLoaded', function () {
-      ng.platform.browser.bootstrap(testCases.isolatedLinkButtons, []);
-  });
-})(window.testCases);

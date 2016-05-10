47935e42c673223977e9b82052f069fce79a7916
Minh Van <minh.van1789@gmail.com>
2016-05-10 17:23:28 +0700

    Refactored ngxButtonComponent

--- components/button/tests/ui/isolated-components/test-case.js
+++ components/button/tests/ui/isolated-components/test-case.js
@@ -1,26 +1,30 @@
-﻿(function (testCases, cores, components, fileService) {
-  testCases.isolatedComponents = ng.core.Component({
-    selector: 'ngx-test-case',
-    templateUrl: fileService.getTestCaseTemplate('button'),
-    directives: [
-      components.ngxButtonComponent
-    ],
-    providers: [
-      cores.ngxColorService,
-      cores.ngxTypeService,
-      cores.ngxSizeService,
-      cores.ngxStateService,
-      components.ngxButtonService
-    ]
-  })
-  .Class(new testCase());
+﻿var ngxButtonComponent = require('./../../../button.component.js');
+var ngxButtonService = require('./../../../services/button.service.js');
+var ngxColorService = require('./../../../../../cores/services/render/color.service.js');
+var ngxSizeService = require('./../../../../../cores/services/render/size.service.js');
+var ngxStateService = require('./../../../../../cores/services/render/state.service.js');
+var ngxBootstrap = require('./../../../../../cores/ngx-bootstrap.js');
+ngxBootstrap = require('./../../../../../cores/ngx-bootstrap.utils.js');
 
-  function testCase() {
-    this.constructor = [components.ngxButtonService, function (ngxButtonService) {
-      this.COLORS = ngxButtonService.getColors();
-      this.TYPES = ngxButtonService.getTypes();
-      this.SIZES = ngxButtonService.getSizes();
-      this.STATES = ngxButtonService.getStates();
-    }];
-  }
-})(window.testCases || (window.testCases = {}), window.ngxBootstrap.ngxCores, window.ngxBootstrap.ngxComponents, ngxBootstrap.configs.fileService);
+function _testCase() {
+  this.constructor = [ngxButtonService, function (ngxButtonService) {
+    this.COLORS = ngxButtonService.getColors();
+    this.SIZES = ngxButtonService.getSizes();
+    this.STATES = ngxButtonService.getStates();
+  }];
+}
+
+module.exports = ng.core.Component({
+  selector: 'ngx-test-case',
+  templateUrl: ngxBootstrap.configs.fileService.getTestCaseTemplate('button'),
+  directives: [
+    ngxButtonComponent
+  ],
+   providers: [
+    ngxColorService,
+    ngxSizeService,
+    ngxStateService,
+    ngxButtonService
+  ]
+})
+.Class(new _testCase());

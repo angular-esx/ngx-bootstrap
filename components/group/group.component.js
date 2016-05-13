ab21ce22e460bd5413bffb931b56e161ae62edfc
Minh Van <minh.van1789@gmail.com>
2016-05-13 18:07:21 +0700

    Refactored ngxGroupComponent

---
+++ components/group/group.component.js
@@ -0,0 +1,43 @@
+﻿var ngxGroupService = require('./services/group.service.js');
+var ngxBaseComponent = require('./../../cores/components/base/base.component.js');
+var ngxRenderService = require('./../../cores/services/render/render.service.js');
+var ngxBootstrap = require('./../../cores/ngx-bootstrap.js');
+ngxBootstrap = require('./../../cores/ngx-bootstrap.utils.js');
+
+function _ngxGroupComponent() {
+  var _base;
+  var _ATTRIBUTES = {
+    TYPE: 'type',
+    SIZE: 'size'
+  };
+
+  this.extends = ngxBaseComponent;
+
+  this.constructor = [
+    ng.core.ElementRef,
+    ngxRenderService,
+    ngxGroupService,
+
+    function ngxGroupComponent(elementRef, ngxRenderService, ngxGroupService) {
+      ngxBaseComponent.apply(this, arguments);
+
+      if (elementRef) {
+        this.ngxGroupService = ngxGroupService;
+      }
+    }
+  ];
+
+  function _getBaseInstance(context) {
+    if (!_base) { _base = context.getBaseInstance(ngxBaseComponent); }
+    return _base;
+  }
+}
+
+module.exports = ng.core.Component({
+  selector: 'ngx-group',
+  templateUrl: 'components/group/templates/group.bootstrap4.html',
+  styleUrls: ['components/group/css/group.bootstrap4.css'],
+  providers: [ngxRenderService],
+  properties: ['type', 'size']
+})
+.Class(new _ngxGroupComponent());
---
+++ components/group/services/group.service.js
@@ -0,0 +1,27 @@
+﻿var ngxTypeService = require('./../../../cores/services/render/type.service.js');
+var ngxSizeService = require('./../../../cores/services/render/size.service.js');
+var ngxBootstrap = require('./../../../cores/ngx-bootstrap.js');
+ngxBootstrap = require('./../../../cores/ngx-bootstrap.utils.js');
+
+function _ngxGroupService() {
+  this.constructor = [
+    ngxTypeService,
+    ngxSizeService,
+
+    function ngxGroupService(ngxTypeService, ngxSizeService) {
+      ngxBootstrap.shallowCopy(this, ngxTypeService);
+      ngxBootstrap.shallowCopy(this, ngxSizeService);
+
+      this.prefixClass = 'ngx-group';
+    }
+  ];
+
+  this.isVerticalTypeClass = function (type) {
+    return this.getTypeClass(type).indexOf(this.getVerticalTypeClass()) > -1;
+  };
+  this.getVerticalTypeClass = function () {
+    return this.prefixClass + '-type-vertical';
+  };
+}
+
+module.exports = ng.core.Class(new _ngxGroupService());

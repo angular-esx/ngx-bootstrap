947bca25f52f3c175cfcd97706d3bf9b68869c9b
Thanh Tran <vanthanh2305@gmail.com>
2016-05-15 02:17:48 +0700

    sass: build single module

--- gulp-tasks/sass.js
+++ gulp-tasks/sass.js
@@ -1,16 +1,30 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+
 module.exports = function (params) {
   return function () {
-    var _gulp = params.gulp,
-        _sass = params.plugins.sass,
-        _rename = params.plugins.rename;
-        _reload = params.plugins.browserSync.reload;
-
-    _gulp.src('./scss/ngx-bootstrap.scss')
-      .pipe(_reload({stream:true}))
-      .pipe(_sass({outputStyle: 'expanded'}).on('error', _sass.logError))
-      .pipe(_gulp.dest('./dist/css/'))
-      .pipe(_sass({outputStyle: 'compressed'}).on('error', _sass.logError))
-      .pipe(_rename('ngx-bootstrap.min.css'))
-      .pipe(_gulp.dest('./dist/css/'));
+    var _componentName = params.args.component,
+        _themeName = params.args.theme,
+    
+    if (_componentName) {
+      _themeName = _themeName || 'bootstrap4';
+        
+      return gulp.src('./components/' + _componentName + '/scss/' + _componentName + '.' + _themeName + '.scss')
+        .pipe(_reload({stream:true}))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('./components/' + _componentName + '/css/'));
+        
+    } else {
+      return gulp.src('./scss/ngx-bootstrap.scss')
+        .pipe(_reload({stream:true}))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename('ngx-bootstrap.min.css'))
+        .pipe(gulp.dest('./dist/css/'));
+        
+    }
+    
   };
 };

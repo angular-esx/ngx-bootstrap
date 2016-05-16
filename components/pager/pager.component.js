7e310ddf418138b95ea9933d2985a4992e208e24
Minh Van <minh.van1789@gmail.com>
2016-05-17 01:44:39 +0700

    Refactored ngxPager

--- components/pager/pager.component.js
+++ components/pager/pager.component.js
@@ -1,42 +1,42 @@
-﻿(function (ngxBootstrap) {
-  ngxBootstrap.ngxComponents.ngxPagerComponent = ng.core.Component({
-    selector: 'ngx-pager',
-    template: _getNgxPagerTemplate(),
-    directives: [
-      ngxBootstrap.ngxCores.ngxLinkComponent
-    ],
-    providers: [ngxBootstrap.ngxCores.ngxRendererService],
-    properties: ['totalPages: total-pages', 'currentPage: current-page', 'showPrevious: show-previous', 'showNext: show-next'],
-    events: ['onSetLinkPage', 'onChangePage']
-  })
-  .Class(new _ngxPager());
+﻿var ngxPagerService = require('./services/pager.service.js');
+var ngxLinkComponent = require('./../../cores/components/link/link.component.js');
+var ngxLinkService = require('./../../cores/components/link/services/link.service.js');
+var ngxBaseComponent = require('./../../cores/components/base/base.component.js');
+var ngxRenderService = require('./../../cores/services/render/render.service.js');
+var ngxBootstrap = require('./../../cores/ngx-bootstrap.js');
+ngxBootstrap = require('./../../cores/ngx-bootstrap.utils.js');
 
-  function _ngxPager() {
-    var _ATTRIBUTES = {
-      TYPE: 'type'
-    };
+function _ngxPagerComponent() {
+  var _base;
+  var _ATTRIBUTES = {
+    TYPE: 'type',
+    TOTAL_PAGES: 'total-pages',
+    CURRENT_PAGE: 'current-page',
+    SHOW_PREVIOUS: 'show-previous',
+    SHOW_NEXT: 'show_next'
+  };
 
-    this.constructor = [
-      ng.core.ElementRef,
-      ngxBootstrap.ngxCores.ngxRendererService,
-      ngxBootstrap.ngxComponents.ngxPagerService,
+  this.extends = ngxBaseComponent;
 
-      function (elementRef, ngxRendererService, ngxPagerService) {
-        this.cssClass = 'pager';
+  this.constructor = [
+    ng.core.ElementRef,
+    ngxRenderService,
+    ngxPagerService,
 
-        this.elementRef = elementRef;
-        this.ngxRendererService = ngxRendererService.for(elementRef.nativeElement);
+    function ngxPagerComponent(elementRef, ngxRenderService, ngxPagerService) {
+      ngxBaseComponent.apply(this, arguments);
+
+      if (elementRef) {
         this.ngxPagerService = ngxPagerService;
 
-        this.onSetLinkPage = new ng.core.EventEmitter();
-        this.onChangePage = new ng.core.EventEmitter(false);
+        this.setLinkPageEmitter = new ng.core.EventEmitter();
+        this.changePageEmitter = new ng.core.EventEmitter(false);
       }
-    ];
+    }
+  ];
 
-    this.ngAfterContentInit = function () {
-      this.type = this.ngxRendererService.getElementAttribute(_ATTRIBUTES.TYPE);
-      this.isAlignedType = this.ngxPagerService.getTypeClass(this.type);
-
+  this.ngOnInit = function () {
+    if (this.elementRef) {
       if (!this.totalPages || this.totalPages < 0) { this.totalPages = 0; }
       else { this.totalPages = parseInt(this.totalPages); }
 
@@ -47,75 +47,86 @@
       if (this.showNext === undefined || this.showNext === null) { this.showNext = true; }
 
       this.pageBuilder = new _pageBuilder();
-      this.pageBuilder.build(this.totalPages, this.currentPage, this.onSetLinkPage);
-    };
+      this.pageBuilder.build(this.totalPages, this.currentPage, this.setLinkPageEmitter);
+    }
+  };
 
-    this.ngAfterViewInit = function () {
-      this.ngxRendererService.addToElementAttribute('class', this.cssClass, true);
-    };
+  this.prev = function ($event) {
+    if (_changePage($event, this.pageBuilder.prevPage, this.changePageEmitter)) {
+      this.currentPage = this.pageBuilder.prevPage.number;
+      this.pageBuilder.build(this.totalPages, this.currentPage, this.setLinkPageEmitter);
+    }
+  };
 
-    this.prev = function ($event) {
-      if (_changePage($event, this.pageBuilder.prevPage, this.onChangePage)) {
-        this.currentPage = this.pageBuilder.prevPage.number;
-        this.pageBuilder.build(this.totalPages, this.currentPage, this.onSetLinkPage);
-      }
-    };
+  this.next = function ($event) {
+    if (_changePage($event, this.pageBuilder.nextPage, this.changePageEmitter)) {
+      this.currentPage = this.pageBuilder.nextPage.number;
+      this.pageBuilder.build(this.totalPages, this.currentPage, this.setLinkPageEmitter);
+    }
+  };
 
-    this.next = function ($event) {
-      if (_changePage($event, this.pageBuilder.nextPage, this.onChangePage)) {
-        this.currentPage = this.pageBuilder.nextPage.number;
-        this.pageBuilder.build(this.totalPages, this.currentPage, this.onSetLinkPage);
-      }
-    };
-
-    function _changePage($event, page, onChangePage) {
-      if (!page || !page.number) {
-        $event.preventDefault();
-        return false;
-      }
-
-      var _isCanceled = false;
-      if (onChangePage) {
-        onChangePage.emit({
-          page: page,
-          cancel: function () { _isCanceled = true; },
-          preventDefault: function () { $event.preventDefault(); }
-        });
-      }
-
-      return !_isCanceled;
+  function _changePage($event, page, onChangePage) {
+    if (!page || !page.number) {
+      $event.preventDefault();
+      return false;
     }
 
-    function _pageBuilder () {
-      this.prevPage = { number: null, link: '#' };
-      this.nextPage = { number: null, link: '#' };
-
-      this.build = function (totalPages, currentPage, onSetLinkPage) {
-        this.prevPage.number = currentPage - 1 > 0 ? currentPage - 1: null;
-        this.prevPage.link = '#';
-        
-        this.nextPage.number = currentPage + 1 <= totalPages ? currentPage + 1 : null;
-        this.nextPage.link = '#';
-
-        var _self = this;
-        onSetLinkPage.emit({
-          currentPage: currentPage,
-          setPrevLink: function (link) {
-            if (link && _self.prevPage.number > 0) { _self.prevPage.link = link; }
-          },
-          setNextLink: function (link) {
-            if (link && _self.nextPage.number <= totalPages) { _self.nextPage.link = link; }
-          }
-        });
-      };
+    var _isCanceled = false;
+    if (onChangePage) {
+      onChangePage.emit({
+        page: page,
+        cancel: function () { _isCanceled = true; },
+        preventDefault: function () { $event.preventDefault(); }
+      });
     }
+
+    return !_isCanceled;
   }
 
-  function _getNgxPagerTemplate() {
-    return [
-      '<ngx-link href="pageBuilder.prevPage.link" [class.pager-prev]="isAlignedType" *ngIf="showPrevious" (click)="prev($event)">Prev</ngx-link>',
-      '<ngx-link href="pageBuilder.nextPage.link" [class.pager-next]="isAlignedType" *ngIf="showNext" (click)="next($event)">Next</ngx-link>',
-    ].join('');
+  function _pageBuilder() {
+    this.prevPage = { number: null, link: '#' };
+    this.nextPage = { number: null, link: '#' };
+
+    this.build = function (totalPages, currentPage, onSetLinkPage) {
+      this.prevPage.number = currentPage - 1 > 0 ? currentPage - 1 : null;
+      this.prevPage.link = '#';
+
+      this.nextPage.number = currentPage + 1 <= totalPages ? currentPage + 1 : null;
+      this.nextPage.link = '#';
+
+      var _self = this;
+      onSetLinkPage.emit({
+        currentPage: currentPage,
+        setPrevLink: function (link) {
+          if (link && _self.prevPage.number > 0) { _self.prevPage.link = link; }
+        },
+        setNextLink: function (link) {
+          if (link && _self.nextPage.number <= totalPages) { _self.nextPage.link = link; }
+        }
+      });
+    };
   }
 
-})(window.ngxBootstrap);
+  function _getBaseInstance(context) {
+    if (!_base) { _base = context.getBaseInstance(ngxBaseComponent); }
+    return _base;
+  }
+}
+
+module.exports = ng.core.Component({
+  selector: 'ngx-pager',
+  templateUrl: 'components/pager/templates/pager.bootstrap4.html',
+  styleUrls: ['components/pager/css/pager.bootstrap4.css'],
+  directives: [ngxLinkComponent],
+  providers: [ngxRenderService, ngxLinkService],
+  properties: [
+    'type',
+    'totalPages: total-pages',
+    'currentPage: current-page',
+    'showPrevious: show-previous',
+    'showNext: show-next',
+    'prefixClass: prefix-class'
+  ],
+  events: ['setLinkPageEmitter: setLinkPage', 'changePageEmitter: changePage']
+})
+.Class(new _ngxPagerComponent());

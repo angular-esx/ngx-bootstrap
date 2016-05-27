9f6aa282bed9c3d14257e6be13f969d25d3c8c5a
Minh Van <minh.van1789@gmail.com>
2016-05-27 21:09:41 +0700

    Init ngxListComponent

---
+++ components/list/services/list.service.js
@@ -0,0 +1,18 @@
+﻿var ngxColorService = require('./../../../cores/services/color.service.js');
+var ngxStateService = require('./../../../cores/services/state.service.js');
+var ngxBootstrap = require('./../../../' + __NGX_BOOTSTRAP__);
+ngxBootstrap = require('./../../../' + __NGX_BOOTSTRAP_UTILS__);
+
+function _ngxListService() {
+  this.constructor = [
+    ngxColorService,
+    ngxStateService,
+
+    function ngxListService(ngxColorService, ngxStateService) {
+      ngxBootstrap.shallowCopy(this, ngxColorService);
+      ngxBootstrap.shallowCopy(this, ngxStateService);
+    }
+  ];
+}
+
+module.exports = ng.core.Class(new _ngxListService());
---
+++ components/list/tests/ui/isolated-components/test-case.js
@@ -0,0 +1,42 @@
+﻿var ngxListComponent = require('./../../../' + __COMPONENT_FILE__);
+var ngxListItemDirective = require('./../../../list-item.directive.js');
+var ngxListService = require('./../../../services/list.service.js');
+var ngxColorService = require('./../../../../../' + __COLOR_SERVICE__);
+var ngxTypeService = require('./../../../../../cores/services/type.service.js');
+var ngxSizeService = require('./../../../../../cores/services/size.service.js');
+var ngxStateService = require('./../../../../../cores/services/state.service.js');
+var ngxLinkComponent = require('./../../../../../cores/components/link/link.component.js');
+var ngxLinkService = require('./../../../../../cores/components/link/services/link.service.js');
+var ngxItemComponent = require('./../../../../../cores/components/item/item.component.js');
+var ngxItemService = require('./../../../../../cores/components/item/services/item.service.js');
+var ngxWindowService = require('./../../../../../cores/services/window.service.js');
+var ngxBootstrap = require('./../../../../../' + __NGX_BOOTSTRAP__);
+ngxBootstrap = require('./../../../../../' + __NGX_BOOTSTRAP_UTILS__);
+
+function _testCase() {
+  this.constructor = [ngxListService, function (ngxListService) {
+    this.COLORS = ngxListService.getColors();
+  }];
+}
+
+module.exports = ng.core.Component({
+  selector: 'ngx-test-case',
+  templateUrl: 'components/list/tests/ui/isolated-components/test-case.html',
+  directives: [
+    ngxListComponent,
+    ngxLinkComponent,
+    ngxItemComponent,
+    ngxListItemDirective
+  ],
+  providers: [
+    ngxColorService,
+    ngxTypeService,
+    ngxSizeService,
+    ngxStateService,
+    ngxWindowService,
+    ngxListService,
+    ngxLinkService,
+    ngxItemService
+  ]
+})
+.Class(new _testCase());

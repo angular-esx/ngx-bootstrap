7f5dcbea98147c5a81e562fb44f567d6609c70e8
Minh Van <minh.van1789@gmail.com>
2016-05-28 22:00:49 +0700

    Init ngxCollapseComponent

---
+++ components/collapse/services/collapse.service.js
@@ -0,0 +1,59 @@
+﻿var ngxStateService = require('./../../../cores/services/state.service.js');
+var ngxAnimationService = require('./../../../cores/services/animation.service.js');
+var ngxBootstrap = require('./../../../' + __NGX_BOOTSTRAP__);
+ngxBootstrap = require('./../../../' + __NGX_BOOTSTRAP_UTILS__);
+
+function _ngxCollapseService() {
+  var _observer;
+  var _ACTIONS = {
+    TOGGLE_COLLAPSE: 'TOGGLE_COLLAPSE',
+    SHOW_COLLAPSE: 'SHOW_COLLAPSE',
+    HIDE_COLLAPSE: 'HIDE_COLLAPSE'
+  };
+
+  this.constructor = [
+    ngxStateService,
+    ngxAnimationService,
+
+    function ngxCollapseService(ngxStateService, ngxAnimationService) {
+      ngxBootstrap.shallowCopy(this, ngxStateService);
+      ngxBootstrap.shallowCopy(this, ngxAnimationService);
+
+      this.ngxCollapse$ = new Rx.Observable(function (observer) {
+        _observer = observer;
+      })
+     .share();
+    }
+  ];
+
+  this.getActions = function () {
+    return ngxBootstrap.shallowCopy({}, _ACTIONS);
+  };
+
+  this.next = function (event) {
+    _observer.next(event);
+  };
+
+  this.getToggle$ = function (collapseId) {
+    return Rx.Observable.from([{ id: collapseId, type: _ACTIONS.TOGGLE_COLLAPSE }]);
+  };
+  this.toggle = function (collapseId) {
+    _observer.next({ id: collapseId, type: _ACTIONS.TOGGLE_COLLAPSE });
+  };
+
+  this.getShow$ = function (collapseId, groupId) {
+    return Rx.Observable.from([{ id: collapseId, group: groupId, type: _ACTIONS.SHOW_COLLAPSE }]);
+  };
+  this.show = function (collapseId, groupId) {
+    _observer.next({ id: collapseId, group: groupId, type: _ACTIONS.SHOW_COLLAPSE });
+  };
+
+  this.getHide$ = function (collapseId, groupId) {
+    return Rx.Observable.from([{ id: collapseId, group: groupId, type: _ACTIONS.HIDE_COLLAPSE }]);
+  };
+  this.hide = function (collapseId, groupId) {
+    _observer.next({ id: collapseId, group: groupId, type: _ACTIONS.HIDE_COLLAPSE });
+  };
+}
+
+module.exports = ng.core.Class(new _ngxCollapseService());
---
+++ components/collapse/tests/ui/isolated-components/test-case.js
@@ -0,0 +1,40 @@
+﻿var ngxCollapseComponent = require('./../../../' + __COMPONENT_FILE__);
+var ngxToggleCollapseDirective = require('./../../../toggle-collapse.directive.js');
+var ngxCollapseService = require('./../../../services/collapse.service.js');
+var ngxColorService = require('./../../../../../' + __COLOR_SERVICE__);
+var ngxTypeService = require('./../../../../../cores/services/type.service.js');
+var ngxSizeService = require('./../../../../../cores/services/size.service.js');
+var ngxStateService = require('./../../../../../cores/services/state.service.js');
+var ngxAnimationService = require('./../../../../../cores/services/animation.service.js');
+var ngxButtonComponent = require('./../../../../../components/button/button.component.bootstrap4.js');
+var ngxButtonService = require('./../../../../../components/button/services/button.service.js');
+var ngxWindowService = require('./../../../../../cores/services/window.service.js');
+var ngxBootstrap = require('./../../../../../' + __NGX_BOOTSTRAP__);
+ngxBootstrap = require('./../../../../../' + __NGX_BOOTSTRAP_UTILS__);
+
+function _testCase() {
+  this.constructor = [ngxCollapseService, function (ngxCollapseService) {
+    this.STATES = ngxCollapseService.getStates();
+  }];
+}
+
+module.exports = ng.core.Component({
+  selector: 'ngx-test-case',
+  templateUrl: 'components/collapse/tests/ui/isolated-components/test-case.html',
+  directives: [
+    ngxCollapseComponent,
+    ngxButtonComponent,
+    ngxToggleCollapseDirective
+  ],
+  providers: [
+    ngxColorService,
+    ngxTypeService,
+    ngxSizeService,
+    ngxStateService,
+    ngxAnimationService,
+    ngxWindowService,
+    ngxCollapseService,
+    ngxButtonService
+  ]
+})
+.Class(new _testCase());

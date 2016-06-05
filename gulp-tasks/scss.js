a1d4de3a92a11b2aff14a638b5fae2c3fd83906c
Thanh Tran <vanthanh2305@gmail.com>
2016-06-05 17:07:12 +0700

    gulp task build core sass

--- gulp-tasks/scss.js
+++ gulp-tasks/scss.js
@@ -2,30 +2,58 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
 var reload = require('browser-sync').reload;
+var fs = require('fs');
 
 module.exports = function (params) {
   return function () {
     var _componentName = params.args.component,
-        _themeName = params.args.theme;
+      _themeName = params.args.theme || 'bootstrap4';
+    
+    var CORE_SASS = './cores/scss/ngx-bootstrap.' + _themeName + '.scss';
+    var contents = addSCSS(CORE_SASS);
     
     if (_componentName) {
-      _themeName = _themeName || 'bootstrap4';
-        
-      return gulp.src('./components/' + _componentName + '/scss/' + _componentName + '.' + _themeName + '.scss')
-        .pipe(reload({stream:true}))
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      
+      var scssPath = fs.readdirSync('./components/' + _componentName)
+        .filter(function (directive) {
+          return directive.match(/directive.js$/g);
+        }).map(function (directive) {
+          var directiveName = directive.replace('.directive.js', '');
+          return './components/' + _componentName + '/scss/' + directive.replace('.directive.js', '') + '.' + _themeName + '.scss';
+        }).filter(function (directive) {
+          try {
+            var stats = fs.statSync(directive);
+            return stats.isFile();
+          } catch (e) {
+            return false;
+          }
+        }).forEach(function(scss){
+          contents += addSCSS(scss);
+          fs.writeFileSync('./scss/ngx-bootstrap.scss', contents, {encoding: 'utf8'});
+        })
+        ;
+      
+      gulp.src('./components/' + _componentName + '/scss/' + _componentName + '.' + _themeName + '.scss')
+        .pipe(reload({ stream: true }))
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
         .pipe(gulp.dest('./components/' + _componentName + '/css/'));
-        
+
     } else {
-      return gulp.src('./scss/ngx-bootstrap.scss')
-        .pipe(reload({stream:true}))
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css/'))
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename('ngx-bootstrap.min.css'))
-        .pipe(gulp.dest('./dist/css/'));
-        
+      
+
     }
     
+    return gulp.src('./scss/ngx-bootstrap.scss')
+        .pipe(reload({ stream: true }))
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css/'))
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(rename('ngx-bootstrap.min.css'))
+        .pipe(gulp.dest('./dist/css/'));
+
   };
 };
+
+function addSCSS(scssName) {
+  return "@import " + '"' + scssName + '";' + "\n";
+}

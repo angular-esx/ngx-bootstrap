03a81bd09851f71dd6bc0aa51d9eb07606698fee
Minh Van <minh.van1789@gmail.com>
2016-06-16 16:27:37 +0700

    Init bootstrap theme for ngxTooltip

---
+++ components/tooltip/index.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+  DIRECTIVES: [
+    require('./tooltip.component.js'),
+    require('./tooltip.directive.js'),
+    require('./tooltip-template.directive.js')
+  ],
+  SERVICE: require('./services/tooltip.service.js')
+};

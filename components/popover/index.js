23ffad95dc6a45bbf5fd1105f5506d5fe5c3bc5c
Minh Van <minh.van1789@gmail.com>
2016-06-16 18:03:55 +0700

    Init bootstrap theme for ngxPopover

---
+++ components/popover/index.js
@@ -0,0 +1,8 @@
+﻿module.exports = {
+  DIRECTIVES: [
+    require('./popover.component.js'),
+    require('./popover.directive.js'),
+    require('./popover-template.directive.js')
+  ],
+  SERVICE: require('./services/popover.service.js')
+};

98daadcf20e2533af95a6c0eda6d3a83ed097830
Minh Van <minh.van1789@gmail.com>
2016-06-27 15:16:39 +0700

    Updated test-ui ngxButton

--- components/button/tests/ui/isolated-components/test-case.js
+++ components/button/tests/ui/isolated-components/test-case.js
@@ -1,14 +1,8 @@
-﻿var ngxButtonComponent = require('./../../../' + __COMPONENT_FILE__);
-var ngxButtonService = require('./../../../services/button.service.js');
-var ngxColorService = require('./../../../../../cores/services/color.service.js');
-var ngxSizeService = require('./../../../../../cores/services/size.service.js');
-var ngxStateService = require('./../../../../../cores/services/state.service.js');
-var ngxWindowService = require('./../../../../../cores/services/window.service.js');
-var ngxBootstrap = require('./../../../../../cores/ngx-bootstrap.js');
-ngxBootstrap = require('./../../../../../cores/ngx-bootstrap.utils.js');
+﻿var NGX_BUTTON = require('components/button/index.js');
+var NGX_CORE_SERVICES = require('coreService');
 
 function _testCase() {
-  this.constructor = [ngxButtonService, function (ngxButtonService) {
+  this.constructor = [NGX_BUTTON.SERVICE, function (ngxButtonService) {
     this.COLORS = ngxButtonService.getColors();
   }];
 
@@ -21,14 +15,11 @@ module.exports = ng.core.Component({
   selector: 'ngx-test-case',
   templateUrl: 'components/button/tests/ui/isolated-components/test-case.html',
   directives: [
-    ngxButtonComponent
+    NGX_BUTTON.DIRECTIVES
   ],
-   providers: [
-    ngxColorService,
-    ngxSizeService,
-    ngxStateService,
-    ngxWindowService,
-    ngxButtonService
+  providers: [
+    NGX_CORE_SERVICES,
+    NGX_BUTTON.SERVICE
   ]
 })
 .Class(new _testCase());

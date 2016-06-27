3a91c38ff3b764c60451e3dc4b4b79eb93454545
Minh Van <minh.van1789@gmail.com>
2016-06-27 16:13:02 +0700

    Updated test-ui for ngxTabs

---
+++ components/tabs/index.js
@@ -0,0 +1,9 @@
+﻿module.exports = {
+  DIRECTIVES: [
+    require('./tabs.component.js'),
+    require('./tab.directive.js'),
+    require('./tab-head.directive.js'),
+    require('./tab-content.directive.js')
+  ],
+  SERVICE: require('./services/tabs.service.js')
+};

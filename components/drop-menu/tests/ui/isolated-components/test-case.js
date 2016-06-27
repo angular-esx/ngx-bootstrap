bc3a574d1699c6549b44645abe8b7dcb53d8a56d
Minh Van <minh.van1789@gmail.com>
2016-06-27 15:25:02 +0700

    Updated test-ui for ngxDropMenu

--- components/drop-menu/tests/ui/isolated-components/test-case.js
+++ components/drop-menu/tests/ui/isolated-components/test-case.js
@@ -1,24 +1,11 @@
-﻿var ngxDropMenuComponent = require('components/drop-menu/drop-menu.component.js');
-var ngxDropMenuService = require('components/drop-menu/services/drop-menu.service.js');
-var ngxDropMenuItemDirective = require('components/drop-menu/drop-menu-item.directive.js');
-var ngxDropMenuItemService = require('components/drop-menu/services/drop-menu-item.service.js');
-var ngxToggleDropdownDirective = require('components/drop-menu/toggle-dropdown.directive.js');
-var ngxToggleDropupDirective = require('components/drop-menu/toggle-dropup.directive.js');
-var ngxGroupComponent = require('components/group/group.component.js');
-var ngxGroupService = require('components/group/services/group.service.js');
-var ngxButtonComponent = require('components/button/button.component.js');
-var ngxButtonService = require('components/button/services/button.service.js');
-var ngxLinkComponent = require('cores/components/link/link.component.js');
-var ngxLinkService = require('cores/components/link/services/link.service.js');
-var ngxItemComponent = require('cores/components/item/item.component.js');
-var ngxItemService = require('cores/components/item/services/item.service.js');
-var ngxTypeService = require('cores/services/type.service.js');
-var ngxCoreService = require('coreService');
-var ngxBootstrap = require('ngxBootstrap');
-ngxBootstrap = require('utils');
+﻿var NGX_DROP_MENU = require('components/drop-menu/index.js');
+var NGX_GROUP = require('components/group/index.js');
+var NGX_BUTTON = require('components/button/index.js');
+var NGX_LINK = require('components/link/index.js');
+var NGX_CORE_SERVICES = require('coreService');
 
 function _testCase() {
-  this.constructor = [ngxDropMenuService, function (ngxDropMenuService) {
+  this.constructor = [NGX_DROP_MENU.SERVICE, function (ngxDropMenuService) {
     this.href = 'https://translate.google.com.vn';
 
     this.STATES = ngxDropMenuService.getStates();
@@ -33,24 +20,18 @@ module.exports = ng.core.Component({
   selector: 'ngx-test-case',
   templateUrl: 'components/drop-menu/tests/ui/isolated-components/test-case.html',
   directives: [
-    ngxLinkComponent,
-    ngxItemComponent,
-    ngxGroupComponent,
-    ngxButtonComponent,
-    ngxDropMenuComponent,
-    ngxDropMenuItemDirective,
-    ngxToggleDropdownDirective,
-    ngxToggleDropupDirective
+    NGX_GROUP.DIRECTIVES,
+    NGX_BUTTON.DIRECTIVES,
+    NGX_LINK.DIRECTIVES,
+    NGX_DROP_MENU.DIRECTIVES
   ],
   providers: [
-   ngxTypeService,
-   ngxGroupService,
-   ngxButtonService,
-   ngxLinkService,
-   ngxItemService,
-   ngxDropMenuItemService,
-   ngxDropMenuService,
-   ngxCoreService
+   NGX_CORE_SERVICES,
+   NGX_DROP_MENU.SERVICE,
+   NGX_DROP_MENU.ITEM_SERVICE,
+   NGX_GROUP.SERVICE,
+   NGX_BUTTON.SERVICE,
+   NGX_LINK.SERVICE
   ]
 })
 .Class(new _testCase());

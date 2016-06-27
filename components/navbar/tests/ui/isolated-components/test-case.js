bcf7fd5727d2bd1e90e800f174a6db50d3632282
Minh Van <minh.van1789@gmail.com>
2016-06-27 15:44:57 +0700

    Updated test-ui for ngxNavbar

--- components/navbar/tests/ui/isolated-components/test-case.js
+++ components/navbar/tests/ui/isolated-components/test-case.js
@@ -1,18 +1,9 @@
-﻿var ngxNavbarComponent = require('components/navbar/navbar.component.js');
-var ngxNavbarBrandDirective = require('components/navbar/navbar-brand.directive.js');
-var ngxNavbarItemDirective = require('components/navbar/navbar-item.directive.js');
-var ngxNavbarDividerDirective = require('components/navbar/navbar-divider.directive.js');
-var ngxNavbarService = require('components/navbar/services/navbar.service.js');
-var ngxCoreService = require('coreService');
-var ngxLinkComponent = require('cores/components/link/link.component.js');
-var ngxLinkService = require('cores/components/link/services/link.service.js');
-var ngxItemComponent = require('cores/components/item/item.component.js');
-var ngxItemService = require('cores/components/item/services/item.service.js');
-var ngxBootstrap = require('ngxBootstrap');
-ngxBootstrap = require('utils');
+﻿var NGX_NAVBAR = require('components/navbar/index.js');
+var NGX_LINK = require('components/link/index.js');
+var NGX_CORE_SERVICES = require('coreService');
 
 function _testCase() {
-  this.constructor = [ngxNavbarService, function (ngxNavbarService) {
+  this.constructor = [NGX_NAVBAR.SERVICE, function (ngxNavbarService) {
     this.COLORS = ngxNavbarService.getColors();
   }];
 }
@@ -21,18 +12,13 @@ module.exports = ng.core.Component({
   selector: 'ngx-test-case',
   templateUrl: 'components/navbar/tests/ui/isolated-components/test-case.html',
   directives: [
-    ngxNavbarComponent,
-    ngxLinkComponent,
-    ngxItemComponent,
-    ngxNavbarBrandDirective,
-    ngxNavbarItemDirective,
-    ngxNavbarDividerDirective
+    NGX_NAVBAR.DIRECTIVES,
+    NGX_LINK.DIRECTIVES
   ],
   providers: [
-    ngxCoreService,
-    ngxNavbarService,
-    ngxLinkService,
-    ngxItemService
+    NGX_CORE_SERVICES,
+    NGX_NAVBAR.SERVICE,
+    NGX_LINK.SERVICE
   ]
 })
 .Class(new _testCase());

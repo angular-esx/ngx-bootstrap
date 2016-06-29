3a3a81e1d4e769ace5d4b3331736e3b22775d6b4
Minh Van <minh.van1789@gmail.com>
2016-06-29 16:24:30 +0700

    Updated template for ngxList. Don't use list-item-content.component.js any more

--- components/list/index.js
+++ components/list/index.js
@@ -1,8 +1,7 @@
 ﻿module.exports = {
   DIRECTIVES: [
     require('./list.component.js'),
-    require('./list-item.component.js'),
-    require('./list-item-content.component.js')
+    require('./list-item.component.js')
   ],
   SERVICES: [require('./services/list-item.service.js')],
   ITEM_SERVICE: require('./services/list-item.service.js')
--- components/list/services/list-item.service.js
+++ components/list/services/list-item.service.js
@@ -1,44 +1,17 @@
 ﻿var ngxColorService = require('colorService');
 var ngxStateService = require('stateService');
-var ngxTypeService = require('typeService');
-var ngxPositionService = require('positionService');
 var ngxBootstrap = require('utils');
 
 function _ngxListItemService() {
   this.constructor = [
     ngxColorService,
     ngxStateService,
-    ngxTypeService,
-    ngxPositionService,
 
-    function ngxListItemService(ngxColorService, ngxStateService, ngxTypeService, ngxPositionService) {
+    function ngxListItemService(ngxColorService, ngxStateService) {
       ngxBootstrap.shallowCopy(this, ngxColorService);
       ngxBootstrap.shallowCopy(this, ngxStateService);
-      ngxBootstrap.shallowCopy(this, ngxTypeService);
-      ngxBootstrap.shallowCopy(this, ngxPositionService);
     }
   ];
-
-  this.isLeftTypeClass = function (prefixClass, type) {
-    return this.getTypeClass(prefixClass, type).indexOf(this.getLeftTypeClass(prefixClass)) > -1;
-  };
-  this.getLeftTypeClass = function (prefixClass) {
-    return prefixClass + '-type-left';
-  };
-
-  this.isCenterTypeClass = function (prefixClass, type) {
-    return this.getTypeClass(prefixClass, type).indexOf(this.getCenterTypeClass(prefixClass)) > -1;
-  };
-  this.getCenterTypeClass = function (prefixClass) {
-    return prefixClass + '-type-center';
-  };
-
-  this.isRightTypeClass = function (prefixClass, type) {
-    return this.getTypeClass(prefixClass, type).indexOf(this.getRightTypeClass(prefixClass)) > -1;
-  };
-  this.getRightTypeClass = function (prefixClass) {
-    return prefixClass + '-type-right';
-  };
 }
 
 module.exports = ng.core.Class(new _ngxListItemService());

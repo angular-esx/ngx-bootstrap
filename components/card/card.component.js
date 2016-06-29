08f43ac3d1230caba940e7ed24790be4cbe068f1
Minh Van <minh.van1789@gmail.com>
2016-06-29 16:02:51 +0700

    Init bootstrap theme for ngxCard

---
+++ components/card/card.component.js
@@ -0,0 +1,32 @@
+﻿var ngxRenderService = require('renderService');
+var ngxBaseComponent = require('baseComponent');
+var ngxBootstrap = require('utils');
+
+function _ngxCardComponent() {
+  var _base;
+
+  this.extends = ngxBaseComponent;
+
+  this.constructor = [
+    ng.core.ElementRef,
+    ngxRenderService,
+
+    function ngxLabelComponent(elementRef, ngxRenderService) {
+      ngxBaseComponent.apply(this, arguments);
+    }
+  ];
+
+  function _getBaseInstance(context) {
+    if (!_base) { _base = context.getBaseInstance(ngxBaseComponent); }
+    return _base;
+  }
+}
+
+module.exports = ng.core.Component({
+  selector: 'ngx-card',
+  template: require('./themes/' + __THEME__ + '/templates/card.html'),
+  styles: [require('./themes/' + __THEME__ + '/scss/card.scss')],
+  providers: [ngxRenderService],
+  properties: ['prefixClass:prefix-class']
+})
+.Class(new _ngxCardComponent());
---
+++ components/card/tests/ui/isolated-components/test-case.js
@@ -0,0 +1,18 @@
+﻿var NGX_CARD = require('components/card/index.js');
+var NGX_CORE_SERVICES = require('coreService');
+
+function _testCase() {
+  this.constructor = function () {};
+}
+
+module.exports = ng.core.Component({
+  selector: 'ngx-test-case',
+  templateUrl: 'components/card/tests/ui/isolated-components/test-case.html',
+  directives: [
+    NGX_CARD.DIRECTIVES
+  ],
+  providers: [
+    NGX_CORE_SERVICES
+  ]
+})
+.Class(new _testCase());

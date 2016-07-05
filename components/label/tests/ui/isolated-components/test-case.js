1e161c5e81fc5781a7ef31f55c4f845b24262b86
Thanh Tran <vanthanh2305@gmail.com>
2016-07-05 23:14:28 +0700

    label: rename to NGX_CORE_SERVICES

--- components/label/tests/ui/isolated-components/test-case.js
+++ components/label/tests/ui/isolated-components/test-case.js
@@ -1,5 +1,5 @@
 ﻿var NGX_LABEL = require('components/label/index.js');
-var NGX_CORE_SERVICE = require('coreService');
+var NGX_CORE_SERVICES = require('coreService');
 
 function _testCase() {
   this.constructor = [NGX_LABEL.SERVICE, function (ngxLabelService) {
@@ -16,7 +16,7 @@ module.exports = ng.core.Component({
   ],
   providers: [
     NGX_LABEL.SERVICE,
-    NGX_CORE_SERVICE
+    NGX_CORE_SERVICES
   ]
 })
 .Class(new _testCase());

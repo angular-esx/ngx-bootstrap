9a046f59cf8d012ad1ff3a5cd376f31eab995ed9
Thanh Tran <vanthanh2305@gmail.com>
2016-07-05 22:32:31 +0700

    remove ngxBootstrap alias from alert

--- components/alert/alert.component.js
+++ components/alert/alert.component.js
@@ -1,8 +1,7 @@
 ﻿var ngxAlertService = require('./services/alert.service.js');
 var ngxRenderService = require('renderService');
 var ngxBaseComponent = require('baseComponent');
-var ngxBootstrap = require('ngxBootstrap');
-ngxBootstrap = require('utils');
+var ngxBootstrap = require('utils');
 
 function _ngxAlertComponent() {
   var _base,

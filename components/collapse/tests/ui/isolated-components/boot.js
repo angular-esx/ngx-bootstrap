72345c861ab4cb66627a065b42b37897dc3db9c3
Thanh Tran <vanthanh2305@gmail.com>
2016-07-11 16:44:51 +0700

    fix collapse test-ui

--- components/collapse/tests/ui/isolated-components/boot.js
+++ components/collapse/tests/ui/isolated-components/boot.js
@@ -1,4 +1,23 @@
-var isolatedComponents = require('./test-case.js');
+function _testCase() {
+  this.constructor = [ngxBootstrap.collapse.SERVICE, function (ngxCollapseService) {
+    this.STATES = ngxCollapseService.getStates();
+  }];
+}
+
+var isolatedComponents = ng.core.Component({
+  selector: 'ngx-test-case',
+  templateUrl: 'components/collapse/tests/ui/isolated-components/test-case.html',
+  directives: [
+    ngxBootstrap.collapse.DIRECTIVES,
+    ngxBootstrap.button.DIRECTIVES,
+  ],
+  providers: [
+    ngxBootstrap.coreService,
+    ngxBootstrap.collapse.SERVICE,
+    ngxBootstrap.button.SERVICE
+  ]
+})
+.Class(new _testCase());
 
 document.addEventListener('DOMContentLoaded', function () {
   ng.platform.browser.bootstrap(isolatedComponents, []);

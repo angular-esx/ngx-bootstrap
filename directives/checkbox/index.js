becef2cfc638995e88b5d98689dc11ccf312e1dc
Thanh Tran <vanthanh2305@gmail.com>
2016-07-12 11:16:57 +0700

    export directives of checkbox as a array

--- directives/checkbox/index.js
+++ directives/checkbox/index.js
@@ -1,7 +1,7 @@
 module.exports = {
-  DIRECTIVES: {
-    CHECKBOX: require('./checkbox.directive.js'),
-    CHECKBOX_GROUP: require('./checkbox-group.directive.js')
-  },
+  DIRECTIVES: [
+    require('./checkbox.directive.js'),
+    require('./checkbox-group.directive.js')
+  ],
   SERVICE: require('./services/checkbox.service.js')
 };

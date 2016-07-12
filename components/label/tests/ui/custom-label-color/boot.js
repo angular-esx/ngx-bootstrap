b70ed36446573dc995d268c715892f9430c08310
Minh Van <minh.van1789@gmail.com>
2016-07-12 17:41:45 +0700

    Deleted unnessary files for ngxLabel

--- components/label/tests/ui/custom-label-color/boot.js
+++
@@ -1,7 +0,0 @@
-var customLabelColor = require('./test-case.js');
-
-(function (testCases) {
-    document.addEventListener('DOMContentLoaded', function () {
-      ng.platform.browser.bootstrap(customLabelColor, []);
-  });
-})(window.testCases);

c04729231050630fa9ec02c1f863d942db571a60
Thanh Tran <vanthanh2305@gmail.com>
2016-07-12 09:40:52 +0700

    test case link component

--- components/link/tests/ui/isolated-components/boot.js
+++ components/link/tests/ui/isolated-components/boot.js
@@ -1,27 +1,3 @@
-function _testCase() {
-  this.constructor = [ngxBootstrap.link.SERVICE, function (ngxLinkService) {
-    this.COLORS = ngxLinkService.getColors();
-    this.href = "http://www.google.com";
-  }];
-
-  this.click = function (event) {
-    console.log('clicked');
-  };
-}
-
-var isolatedComponents = ng.core.Component({
-  selector: 'ngx-test-case',
-  templateUrl: 'components/link/tests/ui/isolated-components/test-case.html',
-  directives: [
-    ngxBootstrap.link.DIRECTIVES
-  ],
-   providers: [
-    ngxBootstrap.link.SERVICE,
-    ngxBootstrap.coreService
-  ]
-})
-.Class(new _testCase());
-
 document.addEventListener('DOMContentLoaded', function () {
   ng.platform.browser.bootstrap(isolatedComponents, []);
 });

6dc0458fbd8a2190443d95b96ede3a2940890036
Thanh Tran <vanthanh2305@gmail.com>
2016-07-12 07:06:40 +0700

    drop-menu component can work alone

--- components/drop-menu/tests/ui/isolated-components/boot.js
+++ components/drop-menu/tests/ui/isolated-components/boot.js
@@ -1,4 +1,34 @@
-var isolatedComponents = require('./test-case.js');
+function _testCase() {
+  this.constructor = [ngxBootstrap['drop-menu'].SERVICE, function (ngxDropMenuService) {
+    this.href = 'https://translate.google.com.vn';
+
+    this.STATES = ngxDropMenuService.getStates();
+  }];
+
+  this.alert = function (id) {
+    alert('You have just clicked ' + id);
+  };
+}
+
+var isolatedComponents = ng.core.Component({
+  selector: 'ngx-test-case',
+  templateUrl: 'components/drop-menu/tests/ui/isolated-components/test-case.html',
+  directives: [
+    ngxBootstrap.group.DIRECTIVES,
+    ngxBootstrap.button.DIRECTIVES,
+    ngxBootstrap.link.DIRECTIVES,
+    ngxBootstrap['drop-menu'].DIRECTIVES
+  ],
+  providers: [
+   ngxBootstrap.coreService,
+   ngxBootstrap['drop-menu'].SERVICE,
+   ngxBootstrap['drop-menu'].ITEM_SERVICE,
+   ngxBootstrap.group.SERVICE,
+   ngxBootstrap.button.SERVICE,
+   ngxBootstrap.link.SERVICE
+  ]
+})
+.Class(new _testCase());
 
 document.addEventListener('DOMContentLoaded', function () {
   ng.platform.browser.bootstrap(isolatedComponents, []);

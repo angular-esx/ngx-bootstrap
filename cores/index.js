c4af1be4a6be63efe9ea43f50354fa15ea98a826
Thanh Tran <vanthanh2305@gmail.com>
2016-07-13 12:52:55 +0700

    import hide directive to core

--- cores/index.js
+++ cores/index.js
@@ -5,6 +5,8 @@ ngxBootstrap.baseDirective = require('baseDirective');
 ngxBootstrap.transcludeDirective = require('transcludeDirective');
 ngxBootstrap.coreService = require('coreService');
 
+// import directives
 ngxBootstrap.checkbox = require('directive/checkbox/index.js');
+ngxBootstrap.hide = require('directive/hide/index.js');
 
 module.exports = ngxBootstrap;

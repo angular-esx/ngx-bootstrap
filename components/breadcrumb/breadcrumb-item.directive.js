87cd31856d89b5603df3ea64a5ce34f679b15784
Thanh Tran <vanthanh2305@gmail.com>
2016-07-15 23:48:08 +0700

    Separate ngx-breadcrumb to code review

---
+++ components/breadcrumb/breadcrumb-item.directive.js
@@ -0,0 +1,11 @@
+﻿function _ngxBreadcrumbItemDirective() {
+  this.constructor = function ngxBreadcrumbItemDirective() { };
+}
+
+module.exports = ng.core.Directive({
+  selector: '[ngx-breadcrumb-item]',
+  host: {
+    '[class.ngx-breadcrumb-item]': 'true'
+  }
+})
+.Class(new _ngxBreadcrumbItemDirective());

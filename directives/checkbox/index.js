e00a1e4785f799c4bb639d51af2e7690fb8824fc
Thanh Tran <vanthanh2305@gmail.com>
2016-07-15 15:38:44 +0700

    update export checkbox directive

--- directives/checkbox/index.js
+++ directives/checkbox/index.js
@@ -1,7 +1,11 @@
-module.exports = {
+var ngxBootstrap = require('ngxBootstrap');
+
+ngxBootstrap.checkbox = {
   DIRECTIVES: [
     require('./checkbox.directive.js'),
     require('./checkbox-group.directive.js')
   ],
   SERVICE: require('./services/checkbox.service.js')
 };
+
+module.exports = ngxBootstrap;

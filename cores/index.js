51933279f443b4cff259b4bda42f074c4aacb02b
Thanh Tran <vanthanh2305@gmail.com>
2016-07-15 15:37:20 +0700

    remove import directives to core

--- cores/index.js
+++ cores/index.js
@@ -5,10 +5,4 @@ ngxBootstrap.baseDirective = require('baseDirective');
 ngxBootstrap.transcludeDirective = require('transcludeDirective');
 ngxBootstrap.coreService = require('coreService');
 
-// import directives
-ngxBootstrap.checkbox = require('directive/checkbox/index.js');
-ngxBootstrap.hide = require('directive/hide/index.js');
-ngxBootstrap.radio = require('directive/radio/index.js');
-ngxBootstrap.toggleItem = require('directive/toggle-item/index.js');
-
 module.exports = ngxBootstrap;

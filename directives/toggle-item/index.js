1172deb08be3189eacde5b6c3ac1c629ce284c12
Thanh Tran <vanthanh2305@gmail.com>
2016-07-15 15:39:34 +0700

    update export toggle item directive

--- directives/toggle-item/index.js
+++ directives/toggle-item/index.js
@@ -1,6 +1,10 @@
-module.exports = {
+var ngxBootstrap = require('ngxBootstrap');
+
+ngxBootstrap.toggleItem = {
     DIRECITVES: [
         require('./toggle-item.directive.js')
     ],
-    SERVICE: require('./services/toggle-item.service.js') 
+    SERVICE: require('./services/toggle-item.service.js')
 };
+
+module.exports = ngxBootstrap;

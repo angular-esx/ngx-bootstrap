95f881bf670245ede2ed0af4c0fa360fc40e0046
Thanh Tran <vanthanh2305@gmail.com>
2016-07-17 21:38:21 +0700

    pager: remove require core

--- components/pager/pager.component.js
+++ components/pager/pager.component.js
@@ -1,16 +1,14 @@
-﻿var ngxBaseComponent = require('baseComponent');
-
-function _ngxPagerComponent() {
+﻿function _ngxPagerComponent() {
   var _base;
 
-  this.extends = ngxBaseComponent;
+  this.extends = ngx.core.baseComponent;
 
   this.constructor = [
     ng.core.ElementRef,
     ng.core.Renderer,
 
     function ngxPagerComponent(elementRef, renderer) {
-      ngxBaseComponent.apply(this, arguments);
+      ngx.core.baseComponent.apply(this, arguments);
 
       if (elementRef) {
         this.setPageEmitter = new ng.core.EventEmitter();
@@ -97,7 +95,7 @@ function _ngxPagerComponent() {
   }
 
   function _getBaseInstance(context) {
-    if (!_base) { _base = context.getBaseInstance(ngxBaseComponent); }
+    if (!_base) { _base = context.getBaseInstance(ngx.core.baseComponent); }
     return _base;
   }
 }

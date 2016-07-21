7b8fa9ceae70cf0fd54fa964bb38fb31dc78c3c2
Minh Van <minh.van1789@gmail.com>
2016-07-21 19:09:33 +0700

    Updated test-ui for ngxCard

--- components/card/card.component.js
+++ components/card/card.component.js
@@ -12,6 +12,18 @@
     }
   ];
 
+  this.initDefaultValues = function () {
+    var _styleProperties = this.getStyleProperties(),
+        _changeRecord;
+
+    if (ngx.isEmpty(this.color)) {
+      this.color = 'secondary';
+      _changeRecord = this.buildChangeRecord(_styleProperties.COLOR, this.color);
+    }
+
+    return _changeRecord;
+  };
+
   this.getPrefixClass = function () {
     return 'ngx-card';
   };
@@ -26,6 +38,6 @@ module.exports = ng.core.Component({
   selector: 'ngx-card',
   template: require('./themes/' + __THEME__ + '/templates/card.html'),
   styles: [require('./themes/' + __THEME__ + '/scss/card.scss')],
-  properties: ['initCssClass:class']
+  properties: ['color', 'initCssClass:class']
 })
 .Class(new _ngxCardComponent());

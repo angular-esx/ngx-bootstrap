76808e18dfcda96d5fbb541f470b85f1d8b8509a
Minh Van <minh.van1789@gmail.com>
2016-07-29 03:48:44 +0700

    Added index.js to components & directives folders

---
+++ components/index.js
@@ -0,0 +1,26 @@
+﻿var exporter = require('../configs/exporter.js');
+
+var exports = new exporter()
+.addExports(require('./alert/index.js'))
+.addExports(require('./breadcrumb/index.js'))
+.addExports(require('./button/index.js'))
+.addExports(require('./card/index.js'))
+.addExports(require('./collapse/index.js'))
+.addExports(require('./drop-menu/index.js'))
+.addExports(require('./grid/index.js'))
+.addExports(require('./group/index.js'))
+.addExports(require('./icon/index.js'))
+.addExports(require('./jumbotron/index.js'))
+.addExports(require('./label/index.js'))
+.addExports(require('./link/index.js'))
+.addExports(require('./list/index.js'))
+.addExports(require('./navbar/index.js'))
+.addExports(require('./pager/index.js'))
+.addExports(require('./pagination/index.js'))
+.addExports(require('./popover/index.js'))
+.addExports(require('./progress/index.js'))
+.addExports(require('./tabs/index.js'))
+.addExports(require('./tooltip/index.js'))
+.merge();
+
+module.exports = exports;
---
+++ directives/index.js
@@ -0,0 +1,9 @@
+﻿var exporter = require('../configs/exporter.js');
+
+var exports = new exporter()
+.addExports(require('./checkbox/index.js'))
+.addExports(require('./radio/index.js'))
+.addExports(require('./hide/index.js'))
+.merge();
+
+module.exports = exports;

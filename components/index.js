89e69364cf9b834ccecc6ef2e8b7e3a98b5f628f
Minh Van <minh.van1789@gmail.com>
2016-08-23 23:53:50 +0700

    Updated exports for cores & components

--- components/index.js
+++ components/index.js
@@ -1,25 +1,160 @@
-export * from './alert/index.js';
-export * from './breadcrumb/index.js';
-export * from './button/index.js';
-export * from './card/index.js';
-export * from './collapse/index.js';
-export * from './drop-menu/index.js';
-export * from './grid/index.js';
-export * from './group/index.js';
-export * from './icon/index.js';
-export * from './jumbotron/index.js';
-export * from './label/index.js';
-export * from './link/index.js';
-export * from './list/index.js';
-export * from './navbar/index.js';
-export * from './pager/index.js';
-export * from './pagination/index.js';
-export * from './popover/index.js';
-export * from './progress/index.js';
-export * from './tabs/index.js';
-export * from './tooltip/index.js';
-export * from './hide/index.js';
-export * from './checkbox/index.js';
-export * from './radio/index.js';
-export * from './backdrop/index.js';
-export * from './modal/index.js';
+export {
+  ngxAlertComponent,
+  ngxAlertLinkDirective,
+  ngxAlertService,
+  NGX_ALERT_DIRECTIVES,
+  NGX_ALERT_PROVIDERS
+} from './alert/index.js';
+
+export {
+  ngxBreadcrumbComponent,
+  ngxBreadcrumbItemDirective,
+  NGX_BREADCRUMB_DIRECTIVES
+} from './breadcrumb/index.js';
+
+export {
+  ngxButtonComponent,
+  NGX_BUTTON_DIRECTIVES
+} from './button/index.js';
+
+export {
+  ngxCardComponent,
+  ngxCardHeaderComponent,
+  NGX_CARD_DIRECTIVES
+} from './card/index.js';
+
+export {
+  ngxCollapseComponent,
+  ngxToggleCollapseDirective,
+  ngxCollapseService,
+  NGX_COLLAPSE_DIRECTIVES,
+  NGX_COLLAPSE_PROVIDERS
+} from './collapse/index.js';
+
+export {
+  ngxDropMenuComponent,
+  ngxDropMenuItemDirective,
+  ngxToggleDropdownDirective,
+  ngxToggleDropupDirective,
+  ngxDropMenuService,
+  NGX_DROPMENU_DIRECTIVES,
+  NGX_DROPMENU_PROVIDERS
+} from './drop-menu/index.js';
+
+export {
+  ngxGridComponent,
+  ngxGridRowDirective,
+  ngxGridColumnDirective,
+  NGX_GRID_DIRECTIVES
+} from './grid/index.js';
+
+export {
+  ngxGroupComponent,
+  NGX_GROUP_DIRECTIVES
+} from './group/index.js';
+
+export {
+  ngxIconComponent,
+  ngxIconService,
+  NGX_ICON_DIRECTIVES,
+  NGX_ICON_PROVIDERS
+} from './icon/index.js';
+
+export {
+  ngxJumbotronComponent,
+  NGX_JUMBOTRON_DIRECTIVES
+} from './jumbotron/index.js';
+
+export {
+  ngxLabelComponent,
+  NGX_LABEL_DIRECTIVES
+} from './label/index.js';
+
+export {
+  ngxLinkComponent,
+  NGX_LINK_DIRECTIVES
+} from './link/index.js';
+
+export {
+  ngxListComponent,
+  ngxListItemComponent,
+  NGX_LIST_DIRECTIVES
+} from './list/index.js';
+
+export {
+  ngxNavbarComponent,
+  ngxNavbarItemDirective,
+  ngxNavbarBrandDirective,
+  ngxNavbarDividerDirective,
+  NGX_NAVBAR_DIRECTIVES
+} from './navbar/index.js';
+
+export {
+  ngxPagerComponent,
+  NGX_PAGER_DIRECTIVES
+} from './pager/index.js';
+
+export {
+  ngxPaginationComponent,
+  NGX_PAGINATION_DIRECTIVES
+} from './pagination/index.js';
+
+export {
+  ngxPopoverComponent,
+  ngxPopoverDirective,
+  ngxPopoverTemplateDirective,
+  ngxPopoverService,
+  NGX_POPOVER_DIRECTIVES,
+  NGX_POPOVER_PROVIDERS
+} from './popover/index.js';
+
+export {
+  ngxProgressComponent,
+  NGX_PROGRESS_DIRECTIVES
+} from './progress/index.js';
+
+export {
+  ngxTabsComponent,
+  ngxTabDirective,
+  ngxTabHeaderDirective,
+  ngxTabContentDirective,
+  ngxTabsService,
+  NGX_TABS_DIRECTIVES,
+  NGX_TABS_PROVIDERS
+} from './tabs/index.js';
+
+export {
+  ngxTooltipComponent,
+  ngxTooltipDirective,
+  ngxTooltipTemplateDirective,
+  ngxTooltipService,
+  NGX_TOOLTIP_DIRECTIVES,
+  NGX_TOOLTIP_PROVIDERS
+} from './tooltip/index.js';
+
+export {
+  ngxBaseHideDirective,
+  ngxHideDownDirective,
+  ngxHideUpDirective,
+  NGX_HIDE_DIRECTIVES
+} from './hide/index.js';
+
+export {
+  ngxBackdropComponent,
+  ngxBackdropObserver,
+  ngxBackdropService,
+  NGX_BACKDROP_DIRECTIVES,
+  NGX_BACKDROP_PROVIDERS
+} from './backdrop/index.js';
+
+export {
+  ngxModalComponent,
+  ngxModalHeaderComponent,
+  ngxModalFooterComponent,
+  ngxModalService,
+  NGX_MODAL_DIRECTIVES,
+  NGX_MODAL_PROVIDERS
+} from './modal/index.js';
+
+// export * from './checkbox/index.js';
+// export * from './radio/index.js';
--- cores/index.js
+++ cores/index.js
@@ -1,5 +1,15 @@
 import * as ngxUtils from './utils';
 
-export * from './components';
-export * from './services';
+export {
+  ngxBaseComponent,
+  ngxBaseDirective,
+  ngxBasePortalHost,
+  ngxBasePortal,
+  ngxComponentPortal,
+  ngxDomPortalHost,
+  ngxTemplatePortal,
+  ngxPortalHostDirective,
+  ngxTemplatePortalDirective
+} from './components';
+export { ngxAnimationService } from './services';
 export { ngxUtils };
